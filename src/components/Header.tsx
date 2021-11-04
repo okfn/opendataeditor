05ac3b5bf908e014bee9b1ef7c0b7e7c66221ac8
roll <eskarev@gmail.com>
2021-11-04 17:44:51 +0200

    Improved select file

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { alpha } from '@mui/material/styles'
+import { alpha, styled } from '@mui/material/styles'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
@@ -9,8 +9,6 @@ import HelpOutline from '@mui/icons-material/HelpOutline'
 import InputBase from '@mui/material/InputBase'
 import Grid from '@mui/material/Grid'
 import Box from '@mui/material/Box'
-
-// General
 
 export default function Header() {
   return (
@@ -24,46 +22,15 @@ export default function Header() {
               </Typography>
             </Grid>
             <Grid item xs={7}>
-              <Box
-                sx={{
-                  flexGrow: 1,
-                  position: 'relative',
-                  borderRadius: 'shape.borderRadius',
-                  backgroundColor: alpha('#fff', 0.15),
-                  '&:hover': {
-                    backgroundColor: alpha('#fff', 0.25),
-                  },
-                  marginLeft: 1,
-                  marginRight: 4,
-                }}
-              >
-                <Box
-                  sx={{
-                    padding: [0, 2],
-                    height: '100%',
-                    position: 'absolute',
-                    pointerEvents: 'none',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                  }}
-                >
+              <Search>
+                <SearchIconWrapper>
                   <SearchIcon />
-                </Box>
-                <InputBase
+                </SearchIconWrapper>
+                <StyledInputBase
                   placeholder="Select File…"
-                  sx={{
-                    color: 'inherit',
-                    padding: [1, 1, 1, 0],
-                    paddingLeft: '2em',
-                    width: '100%',
-                    md: {
-                      width: '20ch',
-                    },
-                  }}
                   inputProps={{ 'aria-label': 'search' }}
                 />
-              </Box>
+              </Search>
             </Grid>
             <Grid item xs={2}>
               <Grid container justifyContent="flex-end">
@@ -78,3 +45,45 @@ export default function Header() {
     </Box>
   )
 }
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}))
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}))
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}))
