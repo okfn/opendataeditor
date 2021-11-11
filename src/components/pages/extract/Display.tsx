deab07ca5bb446ac4a4431b7d8eb7c7eca987558
roll <eskarev@gmail.com>
2021-11-11 18:10:57 +0200

    Implemented base extract

--- src/components/pages/extract/Display.tsx
+++ src/components/pages/extract/Display.tsx
@@ -1,9 +1,29 @@
 import * as React from 'react'
+import { DataGrid } from '@mui/x-data-grid'
 
 interface DisplayProps {
   state: any
 }
 
-export default function Display(_props: DisplayProps) {
-  return <div>Display</div>
+export default function Display(props: DisplayProps) {
+  const rows = props.state.rows.map((row: any, index: number) => ({ ...row, id: index }))
+  const columns = props.state.resource.schema.fields.map((field: any) => {
+    return {
+      field: field.name,
+      headerName: field.title || field.name,
+      // TODO: extend the mapping
+      type: field.type === 'number' ? 'number' : 'string',
+    }
+  })
+  return (
+    <div style={{ height: 600, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        rowsPerPageOptions={[5]}
+        sortingOrder={['desc', 'asc', null]}
+        disableSelectionOnClick
+      />
+    </div>
+  )
 }
