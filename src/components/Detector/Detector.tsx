7f3e32a482162703202881dd184728f2ce3e25b0
roll <eskarev@gmail.com>
2021-11-12 18:10:27 +0200

    Improved Detector

--- src/components/Detector/Detector.tsx
+++ src/components/Detector/Detector.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react'
+import noop from 'lodash/noop'
+import isEqual from 'lodash/isEqual'
+import cloneDeep from 'lodash/cloneDeep'
 import TextField from '@mui/material/TextField'
 import FormControl from '@mui/material/FormControl'
 import Typography from '@mui/material/Typography'
@@ -11,16 +14,25 @@ import Box from '@mui/material/Box'
 import { IDetector } from '../../interfaces/detector'
 import * as helpers from '../../helpers'
 
-const detector: IDetector = {
-  bufferSize: 10000,
-  sampleSize: 100,
+interface DetectorProps {
+  detector?: IDetector
+  onSave?: (detector: IDetector) => void
 }
 
-const Context = React.createContext({} as IDetector)
+interface DetectorContext {
+  prev: IDetector
+  next: IDetector
+  onSave: (detector: IDetector) => void
+}
 
-export default function Detector() {
+const Context = React.createContext({} as DetectorContext)
+
+export default function Detector(props: DetectorProps) {
+  const onSave = props.onSave || noop
+  const detector = props.detector || { bufferSize: 10000, sampleSize: 100 }
+  const context = { prev: detector, next: cloneDeep(detector), onSave }
   return (
-    <Context.Provider value={detector}>
+    <Context.Provider value={context}>
       <Grid container>
         <Grid item xs={3}>
           <General />
@@ -38,7 +50,7 @@ export default function Detector() {
 }
 
 function General() {
-  const detector = React.useContext(Context)
+  const context = React.useContext(Context)
   return (
     <FormControl>
       <Typography variant="h6">General</Typography>
@@ -46,16 +58,16 @@ function General() {
         type="number"
         label="Buffer Size"
         inputProps={{ min: 0, step: 10000 }}
-        defaultValue={detector.bufferSize}
-        onChange={(ev) => (detector.bufferSize = parseInt(ev.target.value))}
+        defaultValue={context.next.bufferSize}
+        onChange={(ev) => (context.next.bufferSize = parseInt(ev.target.value))}
         margin="normal"
       />
       <TextField
         type="number"
         label="Sample Size"
         inputProps={{ min: 0, step: 100 }}
-        defaultValue={detector.sampleSize}
-        onChange={(ev) => (detector.sampleSize = parseInt(ev.target.value))}
+        defaultValue={context.next.sampleSize}
+        onChange={(ev) => (context.next.sampleSize = parseInt(ev.target.value))}
         margin="normal"
       />
     </FormControl>
@@ -63,36 +75,36 @@ function General() {
 }
 
 function Field() {
-  const detector = React.useContext(Context)
+  const context = React.useContext(Context)
   return (
     <FormControl>
       <Typography variant="h6">Field</Typography>
       <TextField
         label="Type"
         margin="normal"
-        defaultValue={detector.fieldType}
-        onChange={(ev) => (detector.fieldType = ev.target.value)}
+        defaultValue={context.next.fieldType}
+        onChange={(ev) => (context.next.fieldType = ev.target.value)}
       />
       <TextField
         label="Names"
         margin="normal"
-        defaultValue={(detector.fieldNames || []).join(',')}
-        onChange={(ev) => (detector.fieldNames = ev.target.value.split(','))}
+        defaultValue={(context.next.fieldNames || []).join(',')}
+        onChange={(ev) => (context.next.fieldNames = ev.target.value.split(','))}
       />
     </FormControl>
   )
 }
 
 function Schema() {
-  const detector = React.useContext(Context)
+  const context = React.useContext(Context)
   return (
     <FormControl>
       <Typography variant="h6">Schema</Typography>
       <TextField
         select
         label="Sync"
-        defaultValue={'no'}
-        onChange={(ev) => (detector.schemaSync = ev.target.value === 'yes')}
+        defaultValue={context.next.schemaSync ? 'yes' : 'no'}
+        onChange={(ev) => (context.next.schemaSync = ev.target.value === 'yes')}
         sx={{ width: '30ch' }}
         margin="normal"
       >
@@ -104,17 +116,30 @@ function Schema() {
 }
 
 function Actions() {
-  const detector = React.useContext(Context)
+  const context = React.useContext(Context)
+  const isUpdated = !isEqual(context.next, context.prev)
   return (
     <Box>
       <Divider sx={{ mt: 2, mb: 3 }} />
       <Stack spacing={2} direction="row" sx={{ pl: 0 }}>
-        <Button variant="contained">Save</Button>
-        <Button variant="contained">Restore</Button>
+        <Button
+          variant="contained"
+          disabled={!isUpdated}
+          onClick={() => context.onSave(context.next)}
+        >
+          Save
+        </Button>
+        <Button
+          variant="contained"
+          disabled={!isUpdated}
+          onClick={() => (context.next = cloneDeep(context.prev))}
+        >
+          Restore
+        </Button>
         <Button
           variant="contained"
           download="detector.json"
-          href={helpers.exportDescriptor(detector)}
+          href={helpers.exportDescriptor(context.next)}
         >
           Export
         </Button>
