11a720faf401e4195cbc8b74d3e085408554a289
roll <eskarev@gmail.com>
2021-11-20 13:42:46 +0200

    Improved Resource

--- src/components/Resource/Resource.tsx
+++ src/components/Resource/Resource.tsx
@@ -9,9 +9,11 @@ import FormControl from '@mui/material/FormControl'
 import Typography from '@mui/material/Typography'
 import Divider from '@mui/material/Divider'
 import Button from '@mui/material/Button'
-import Stack from '@mui/material/Stack'
 import Grid from '@mui/material/Grid'
 import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
 import { IResource } from '../../interfaces/resource'
 import * as helpers from '../../helpers'
 
@@ -75,7 +77,10 @@ function Editor() {
   const isPreview = useStore((state) => state.isPreview)
   if (isPreview) return <Preview />
   return (
-    <Grid container>
+    <Grid container spacing={3}>
+      <Grid item xs={3}>
+        <Help />
+      </Grid>
       <Grid item xs={3}>
         <General />
       </Grid>
@@ -98,11 +103,36 @@ function Preview() {
   )
 }
 
+function Help() {
+  return (
+    <Card variant="outlined" sx={{ height: 'calc(100% - 8px)' }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Help
+        </Typography>
+        <Typography variant="h5" component="div">
+          Resource
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          describe
+        </Typography>
+        <Typography variant="body2">
+          Frictionless is a framework to describe, extract, validate, and transform
+          tabular data in Python.
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  )
+}
+
 function General() {
   const resource = useStore((state) => state.next)
   const update = useStore((state) => state.update)
   return (
-    <FormControl>
+    <FormControl sx={{ width: '100%' }}>
       <Typography variant="h6">General</Typography>
       <TextField label="Path" margin="normal" defaultValue={resource.path} disabled />
       <TextField
@@ -132,7 +162,7 @@ function Details() {
   const resource = useStore((state) => state.next)
   const update = useStore((state) => state.update)
   return (
-    <FormControl>
+    <FormControl sx={{ width: '100%' }}>
       <Typography variant="h6">Details</Typography>
       <TextField label="Scheme" margin="normal" disabled defaultValue={resource.scheme} />
       <TextField
@@ -161,7 +191,7 @@ function Stats() {
   const resource = useStore((state) => state.next)
   const keys = ['hash', 'bytes', 'fields', 'rows']
   return (
-    <FormControl>
+    <FormControl sx={{ width: '100%' }}>
       <Typography variant="h6">Stats</Typography>
       {keys.map((key) => (
         <TextField
@@ -179,7 +209,7 @@ function Stats() {
 
 function Actions() {
   const resource = useStore((state) => state.next)
-  const isPreview = useStore((state) => state.isPreview)
+  // const isPreview = useStore((state) => state.isPreview)
   const isUpdated = useStore((state) => state.isUpdated)
   const preview = useStore((state) => state.preview)
   const revert = useStore((state) => state.revert)
@@ -187,28 +217,51 @@ function Actions() {
   return (
     <Box>
       <Divider sx={{ mt: 2, mb: 3 }} />
-      <Stack spacing={2} direction="row" sx={{ pl: 0 }}>
-        <Button
-          variant="contained"
-          download={`${resource.name}.resource.json`}
-          href={helpers.exportDescriptor(resource)}
-        >
-          Export
-        </Button>
-        <Button
-          variant={isPreview ? 'outlined' : 'contained'}
-          onClick={preview}
-          color="warning"
-        >
-          Preview
-        </Button>
-        <Button variant="contained" disabled={!isUpdated} onClick={revert} color="error">
-          Revert
-        </Button>
-        <Button variant="contained" disabled={!isUpdated} onClick={save} color="success">
-          Save
-        </Button>
-      </Stack>
+      <Grid container spacing={3}>
+        <Grid item xs={3}>
+          <Button
+            variant="contained"
+            onClick={preview}
+            sx={{ width: '100%' }}
+            color="info"
+          >
+            Import
+          </Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button
+            variant="contained"
+            download={`${resource.name}.resource.json`}
+            href={helpers.exportDescriptor(resource)}
+            sx={{ width: '100%' }}
+            color="info"
+          >
+            Export
+          </Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button
+            variant="contained"
+            disabled={!isUpdated}
+            onClick={save}
+            color="success"
+            sx={{ width: '100%' }}
+          >
+            Apply
+          </Button>
+        </Grid>
+        <Grid item xs={3}>
+          <Button
+            variant="contained"
+            disabled={!isUpdated}
+            onClick={revert}
+            color="error"
+            sx={{ width: '100%' }}
+          >
+            Revert
+          </Button>
+        </Grid>
+      </Grid>
     </Box>
   )
 }
