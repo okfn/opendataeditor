a9331447f0582828a1cce9d7ab9829a54ff9b671
roll <eskarev@gmail.com>
2021-11-20 11:54:43 +0200

    Drafted help on the home page

--- src/components/Pages/Home.tsx
+++ src/components/Pages/Home.tsx
@@ -1,5 +1,34 @@
 import * as React from 'react'
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
 
 export default function Home() {
-  return <div></div>
+  return (
+    <Box>
+      <Card variant="outlined" sx={{ m: 2 }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            Help
+          </Typography>
+          <Typography variant="h5" component="div">
+            Frictionless Application
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            adjective
+          </Typography>
+          <Typography variant="body2">
+            Frictionless is a framework to describe, extract, validate, and transform
+            tabular data in Python.
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    </Box>
+  )
 }
