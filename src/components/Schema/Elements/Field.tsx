b9a5b7c2ccda59d47a05e1bb9657e19c14b8fb9e
roll <eskarev@gmail.com>
2021-11-27 18:25:52 +0300

    Justified controls

--- src/components/Schema/Elements/Field.tsx
+++ src/components/Schema/Elements/Field.tsx
@@ -10,60 +10,58 @@ export default function Field() {
   if (elementIndex === undefined) return null
   const field = useStore((state) => state.descriptor.fields[elementIndex])
   return (
-    <FormControl fullWidth>
-      <Grid container spacing={3}>
-        <Grid item xs={6}>
-          <FormControl fullWidth>
-            <TextField
-              label="Name"
-              margin="normal"
-              value={field.name}
-              onChange={(ev) => updateField({ name: ev.target.value })}
-            />
-            <Grid container spacing={1}>
-              <Grid item xs={6}>
-                <TextField
-                  label="Type"
-                  margin="normal"
-                  value={field.type}
-                  onChange={(ev) => updateField({ type: ev.target.value })}
-                />
-              </Grid>
-              <Grid item xs={6}>
-                <TextField
-                  label="Format"
-                  margin="normal"
-                  value={field.format}
-                  onChange={(ev) => updateField({ format: ev.target.value })}
-                />
-              </Grid>
+    <Grid container spacing={3}>
+      <Grid item xs={6}>
+        <FormControl fullWidth>
+          <TextField
+            label="Name"
+            margin="normal"
+            value={field.name}
+            onChange={(ev) => updateField({ name: ev.target.value })}
+          />
+          <Grid container spacing={1}>
+            <Grid item xs={6}>
+              <TextField
+                label="Type"
+                margin="normal"
+                value={field.type}
+                onChange={(ev) => updateField({ type: ev.target.value })}
+              />
             </Grid>
-            <TextField
-              label="Title"
-              margin="normal"
-              value={field.title || ''}
-              onChange={(ev) => updateField({ title: ev.target.value })}
-            />
-            <TextField
-              label="Description"
-              margin="normal"
-              value={field.description || ''}
-              onChange={(ev) => updateField({ description: ev.target.value })}
-              multiline
-            />
-          </FormControl>
-        </Grid>
-        <Grid item xs={6}>
-          <FormControl fullWidth>
-            <TextField
-              label="RDF Type"
-              margin="normal"
-              value={field.rdfType || ''}
-              onChange={(ev) => updateField({ rdfType: ev.target.value })}
-            />
-          </FormControl>
-        </Grid>
+            <Grid item xs={6}>
+              <TextField
+                label="Format"
+                margin="normal"
+                value={field.format}
+                onChange={(ev) => updateField({ format: ev.target.value })}
+              />
+            </Grid>
+          </Grid>
+          <TextField
+            label="Title"
+            margin="normal"
+            value={field.title || ''}
+            onChange={(ev) => updateField({ title: ev.target.value })}
+          />
+          <TextField
+            label="Description"
+            margin="normal"
+            value={field.description || ''}
+            onChange={(ev) => updateField({ description: ev.target.value })}
+            multiline
+          />
+        </FormControl>
       </Grid>
-    </FormControl>
+      <Grid item xs={6}>
+        <FormControl fullWidth>
+          <TextField
+            label="RDF Type"
+            margin="normal"
+            value={field.rdfType || ''}
+            onChange={(ev) => updateField({ rdfType: ev.target.value })}
+          />
+        </FormControl>
+      </Grid>
+    </Grid>
   )
 }
