39d3d010b587340fb9f1100d77e089a302581c63
roll <eskarev@gmail.com>
2021-12-02 18:24:48 +0300

    Rebased on inovua grid

--- src/components/Application/Contents/Data.tsx
+++ src/components/Application/Contents/Data.tsx
@@ -1,17 +1,24 @@
 import * as React from 'react'
 import { assert } from 'ts-essentials'
+import Box from '@mui/material/Box'
 import Report from '../../Report'
 import Table from '../../Table'
 import File from '../../File'
 import { useStore } from '../store'
 
+// TODO: rework
+
 export default function Data() {
+  const contentType = useStore((state) => state.contentType)
+  const isMetadataOpen = useStore((state) => state.isMetadataOpen)
   const isSourceView = useStore((state) => state.isSourceView)
   const isReportView = useStore((state) => state.isReportView)
   const resource = useStore((state) => state.resource)
   const rows = useStore((state) => state.rows)
   const text = useStore((state) => state.text)
   const report = useStore((state) => state.report)
+  const footerHeight = contentType === 'data' && isMetadataOpen ? '544px' : '64px'
+  const contentHeight = `calc(100vh - 64px - ${footerHeight})`
   assert(resource)
   assert(rows)
   // TODO: text might not be available while it will be still correct state
@@ -19,5 +26,9 @@ export default function Data() {
   assert(report)
   if (isSourceView) return <File text={text} />
   if (isReportView) return <Report report={report} />
-  return <Table schema={resource.schema} rows={rows} />
+  return (
+    <Box sx={{ m: -2 }}>
+      <Table schema={resource.schema} rows={rows} height={contentHeight} />
+    </Box>
+  )
 }
--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,30 +1,31 @@
 import * as React from 'react'
-import { DataGrid } from '@mui/x-data-grid'
+import ReactDataGrid from '@inovua/reactdatagrid-community'
+import '@inovua/reactdatagrid-community/index.css'
 import { ISchema, IRow } from '../../interfaces'
 
 interface TableProps {
   schema: ISchema
   rows: IRow
+  height?: string
 }
 
 export default function Table(props: TableProps) {
-  const gridRows = props.rows.map((row: any, index: number) => ({ ...row, id: index }))
-  const gridFields = props.schema.fields.map((field: any) => {
+  const height = props.height || '600px'
+  const columns = props.schema.fields.map((field) => {
     return {
-      field: field.name,
-      headerName: field.title || field.name,
-      // TODO: extend the mapping
-      type: field.type === 'number' ? 'number' : 'string',
+      name: field.name,
+      header: field.title || field.name,
+      type: ['integer', 'number'].includes(field.type) ? 'number' : 'string',
     }
   })
+  const dataSource = props.rows as any
   return (
     <div style={{ height: '100%', width: '100%' }}>
-      <DataGrid
-        rows={gridRows}
-        columns={gridFields}
-        rowsPerPageOptions={[5]}
-        sortingOrder={['desc', 'asc', null]}
-        disableSelectionOnClick
+      <ReactDataGrid
+        idProperty="id"
+        columns={columns}
+        dataSource={dataSource}
+        style={{ height, minHeight: height, borderBottom: 'none' }}
       />
     </div>
   )
