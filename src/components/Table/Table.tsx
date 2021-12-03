eeffd20b35a525ddbf250e3d4087b70564cabe7e
roll <eskarev@gmail.com>
2021-12-03 09:10:35 +0300

    Improved Table performance

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -11,20 +11,12 @@ interface TableProps {
 
 export default function Table(props: TableProps) {
   const height = props.height || '600px'
-  const columns = props.schema.fields.map((field) => {
-    return {
-      name: field.name,
-      header: field.title || field.name,
-      type: ['integer', 'number'].includes(field.type) ? 'number' : 'string',
-    }
-  })
-  const dataSource = props.rows as any
   return (
     <div style={{ height: '100%', width: '100%' }}>
       <ReactDataGrid
-        idProperty="id"
-        columns={columns}
-        dataSource={dataSource}
+        idProperty="table"
+        columns={props.schema.fields}
+        dataSource={props.rows as any}
         style={{ height, minHeight: height, borderBottom: 'none' }}
       />
     </div>
