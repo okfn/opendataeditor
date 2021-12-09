b1540bf019535ec956e82ec241eb44b8e908c62b
roll <eskarev@gmail.com>
2021-12-09 14:10:13 +0300

    Wrap api commands in try/catch

--- src/server.ts
+++ src/server.ts
@@ -47,83 +47,91 @@ export class Server {
   // Actions
 
   protected async describe(request: IRequest, response: IResponse) {
-    if (!request.file) {
-      response.json({ error: true })
-      return
+    try {
+      if (!request.file) {
+        response.json({ error: true })
+        return
+      }
+      const { path, cleanup } = await file({
+        postfix: pathmodule.extname(request.file.originalname),
+      })
+      fs.promises.writeFile(path, request.file.buffer)
+      const command = `frictionless describe ${path} --json --stats --expand`
+      const { stdout } = await promiseExec(command)
+      const resource = JSON.parse(stdout)
+      resource.path = request.file.originalname
+      resource.name = pathmodule.parse(request.file.originalname).name
+      cleanup()
+      response.json({ error: false, resource })
+    } catch (error) {
+      response.json({ error: 'Internal error' })
     }
-    const { path, cleanup } = await file({
-      postfix: pathmodule.extname(request.file.originalname),
-    })
-    fs.promises.writeFile(path, request.file.buffer)
-    const command = `frictionless describe ${path} --json --stats --expand`
-    const { stdout } = await promiseExec(command)
-    const resource = JSON.parse(stdout)
-    resource.path = request.file.originalname
-    resource.name = pathmodule.parse(request.file.originalname).name
-    cleanup()
-    response.json({ error: false, resource })
   }
 
   protected async extract(request: any, response: IResponse) {
-    if (!request.file) {
-      response.json({ error: true })
-      return
+    try {
+      if (!request.file) {
+        response.json({ error: true })
+        return
+      }
+      const dir = await tmp.dir({ unsafeCleanup: true })
+      const resource = JSON.parse(request.body.resource)
+      const sourcePath = `${dir.path}/source.csv`
+      const resourcePath = `${dir.path}/resource.json`
+      resource.path = sourcePath
+      fs.promises.writeFile(sourcePath, request.file.buffer)
+      fs.promises.writeFile(resourcePath, JSON.stringify(resource))
+      const command1 = `frictionless extract ${resourcePath} --trusted --json`
+      const result1 = await promiseExec(command1)
+      const rows = JSON.parse(result1.stdout)
+      response.json({ error: false, rows })
+      dir.cleanup()
+    } catch (error) {
+      response.json({ error: 'Internal error' })
     }
-    const dir = await tmp.dir({ unsafeCleanup: true })
-    const resource = JSON.parse(request.body.resource)
-    const sourcePath = `${dir.path}/source.csv`
-    const resourcePath = `${dir.path}/resource.json`
-    resource.path = sourcePath
-    fs.promises.writeFile(sourcePath, request.file.buffer)
-    fs.promises.writeFile(resourcePath, JSON.stringify(resource))
-    const command1 = `frictionless extract ${resourcePath} --trusted --json`
-    const result1 = await promiseExec(command1)
-    const rows = JSON.parse(result1.stdout)
-    response.json({ error: false, rows })
-    dir.cleanup()
   }
 
   protected async validate(request: any, response: IResponse) {
-    if (!request.file) {
-      response.json({ error: true })
-      return
+    try {
+      const dir = await tmp.dir({ unsafeCleanup: true })
+      const inqiury = JSON.parse(request.body.inquiry)
+      const sourcePath = `${dir.path}/source.csv`
+      const inqiuryPath = `${dir.path}/inquiry.json`
+      inqiury.tasks[0].source.path = sourcePath
+      fs.promises.writeFile(sourcePath, request.file.buffer)
+      fs.promises.writeFile(inqiuryPath, JSON.stringify(inqiury))
+      const command1 = `frictionless validate ${inqiuryPath} --json`
+      const result1 = await promiseExec(command1)
+      const report = JSON.parse(result1.stdout)
+      response.json({ error: false, report })
+      dir.cleanup()
+    } catch (error) {
+      response.json({ error: 'Internal error' })
     }
-    const dir = await tmp.dir({ unsafeCleanup: true })
-    const inqiury = JSON.parse(request.body.inquiry)
-    const sourcePath = `${dir.path}/source.csv`
-    const inqiuryPath = `${dir.path}/inquiry.json`
-    inqiury.tasks[0].source.path = sourcePath
-    fs.promises.writeFile(sourcePath, request.file.buffer)
-    fs.promises.writeFile(inqiuryPath, JSON.stringify(inqiury))
-    const command1 = `frictionless validate ${inqiuryPath} --json`
-    const result1 = await promiseExec(command1)
-    const report = JSON.parse(result1.stdout)
-    response.json({ error: false, report })
-    dir.cleanup()
   }
 
   protected async transform(request: any, response: IResponse) {
-    if (!request.file) {
-      response.json({ error: true })
-      return
+    try {
+      const dir = await tmp.dir({ unsafeCleanup: true })
+      const pipeline = JSON.parse(request.body.pipeline)
+      const sourcePath = `${dir.path}/source.csv`
+      const targetPath = `${dir.path}/target.csv`
+      const pipelinePath = `${dir.path}/pipeline.json`
+      pipeline.tasks[0].source.path = sourcePath
+      pipeline.tasks[0].steps.push({ code: 'table-write', path: targetPath })
+      fs.promises.writeFile(sourcePath, request.file.buffer)
+      fs.promises.writeFile(pipelinePath, JSON.stringify(pipeline))
+      const command1 = `frictionless transform ${pipelinePath} --json`
+      const result1 = await promiseExec(command1)
+      const status = JSON.parse(result1.stdout)
+      const command2 = `frictionless extract ${targetPath} --json`
+      const result2 = await promiseExec(command2)
+      const targetRows = JSON.parse(result2.stdout)
+      response.json({ error: false, status, targetRows })
+      dir.cleanup()
+    } catch (error) {
+      response.json({ error: 'Internal error' })
     }
-    const dir = await tmp.dir({ unsafeCleanup: true })
-    const pipeline = JSON.parse(request.body.pipeline)
-    const sourcePath = `${dir.path}/source.csv`
-    const targetPath = `${dir.path}/target.csv`
-    const pipelinePath = `${dir.path}/pipeline.json`
-    pipeline.tasks[0].source.path = sourcePath
-    pipeline.tasks[0].steps.push({ code: 'table-write', path: targetPath })
-    fs.promises.writeFile(sourcePath, request.file.buffer)
-    fs.promises.writeFile(pipelinePath, JSON.stringify(pipeline))
-    const command1 = `frictionless transform ${pipelinePath} --json`
-    const result1 = await promiseExec(command1)
-    const status = JSON.parse(result1.stdout)
-    const command2 = `frictionless extract ${targetPath} --json`
-    const result2 = await promiseExec(command2)
-    const targetRows = JSON.parse(result2.stdout)
-    response.json({ error: false, status, targetRows })
-    dir.cleanup()
   }
 }
 
