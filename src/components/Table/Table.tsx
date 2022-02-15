9ce652e73d61e740f01d8cf9e74b514412a419a5
roll <eskarev@gmail.com>
2022-02-15 09:52:45 +0300

    Rebase on columns

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -9,12 +9,22 @@ interface TableProps {
 }
 
 export default function Table(props: TableProps) {
+  const { fields } = props.table.schema
   const height = props.height || '600px'
+  const columns = React.useMemo(() => {
+    return fields.map((field) => {
+      return {
+        name: field.name,
+        header: field.title || field.name,
+        type: ['integer', 'number'].includes(field.type) ? 'number' : 'string',
+      }
+    })
+  }, [fields])
   return (
     <div style={{ height: '100%', width: '100%' }}>
       <ReactDataGrid
         idProperty="table"
-        columns={props.table.schema.fields}
+        columns={columns}
         dataSource={props.table.rows as any}
         style={{ height, minHeight: height, borderBottom: 'none' }}
       />
