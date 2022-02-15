27abd7d3e354d3c976b95acf51f03f7d6afe7108
roll <eskarev@gmail.com>
2022-02-15 11:32:05 +0300

    Support label errors

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -30,11 +30,13 @@ export default function Table(props: TableProps) {
     return createErrorIndex(report)
   }, [report])
   const columns = React.useMemo(() => {
-    return fields.map((field) => {
+    return fields.map((field, index) => {
+      const fieldPosition = index + 1
       return {
         name: field.name,
         header: field.title || field.name,
         type: ['integer', 'number'].includes(field.type) ? 'number' : 'string',
+        headerProps: fieldPosition in errorIndex.label ? {style: {backgroundColor: 'red'}} : {},
         onRender: (cellProps: any, context: any) => {
           const {rowIndex, columnIndex} = context
           const rowKey = `${rowIndex + 2}`
@@ -57,14 +59,28 @@ export default function Table(props: TableProps) {
   )
 }
 
+interface IErrorIndex {
+  header: IDict<IError[]>,
+  label: IDict<IError[]>,
+  row: IDict<IError[]>,
+  cell: IDict<IError[]>
+}
+
 function createErrorIndex(report?: IReport) {
-  const errorIndex = {row: {}, cell: {}} as {row: IDict<IError[]>, cell: IDict<IError[]>}
+  const errorIndex: IErrorIndex = {header: {}, label: {}, row: {}, cell: {}}
   if (!report) return errorIndex
   const errorTask = report.tasks[0]
   if (!errorTask) return errorIndex
   for (const error of errorTask.errors) {
-    if (!error.rowPosition) continue
-    if (!error.fieldPosition)  {
+    if (!error.rowPosition && !error.fieldPosition) {
+      const headerKey = '1'
+      errorIndex.header[headerKey] = errorIndex.header[headerKey] || []
+      errorIndex.header[headerKey].push(error)
+    } else if (!error.rowPosition) {
+      const labelKey = `${error.fieldPosition}`
+      errorIndex.label[labelKey] = errorIndex.label[labelKey] || []
+      errorIndex.label[labelKey].push(error)
+    } else if (!error.fieldPosition)  {
       const rowKey = `${error.rowPosition}`
       errorIndex.row[rowKey] = errorIndex.row[rowKey] || []
       errorIndex.row[rowKey].push(error)
