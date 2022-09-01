28ab4b03c6671b3947d3b44ffd8e1518a3dd7661
roll <eskarev@gmail.com>
2022-09-01 15:16:21 +0300

    Fixed File component

--- src/components/File/File.tsx
+++ src/components/File/File.tsx
@@ -1,18 +1,17 @@
 import * as React from 'react'
 import Box from '@mui/material/Box'
-import { IFile } from '../../interfaces'
 
 // TODO: restyle scrolls (use Table's style)
 
 interface FileProps {
-  file: IFile
+  text: string
 }
 
 export default function File(props: FileProps) {
   return (
     <Box sx={{ height: '100%', width: '100%', overflow: 'auto' }}>
       <pre style={{ margin: 0 }}>
-        <code>{props.file.text}</code>
+        <code>{props.text}</code>
       </pre>
     </Box>
   )
