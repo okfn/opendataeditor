7e4392f6a36030054cf66b6af2ef50d75542d994
roll <eskarev@gmail.com>
2022-09-01 11:03:45 +0300

    Wrapped server functions

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,5 +1,8 @@
 import * as React from 'react'
+import omit from 'lodash/omit'
+import { IFile } from './interfaces/file'
 
+// TODO: remove React dependency from helpers
 export function useAsyncReducer(reducer: any, initialState: any) {
   const [state, setState] = React.useState(initialState)
   const dispatchState = async (action: any) => setState(await reducer(state, action))
@@ -11,10 +14,21 @@ export function exportDescriptor(descriptor: object) {
   return `data: text/json;charset=utf-8,${text}`
 }
 
-export async function request(path: string, props: object = {}) {
-  return fetch(path, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json;charset=utf-8' },
-    body: JSON.stringify(props),
-  })
+export async function request(
+  path: string,
+  props: { [key: string]: any; file?: IFile } = {}
+) {
+  const method = 'POST'
+  let headers
+  let body
+  if (props.file) {
+    body = new FormData()
+    body.append('file', new Blob([props.file.bytes]), props.file.name)
+    body.append('detector', JSON.stringify(omit(props, 'file')))
+  } else {
+    headers = { 'Content-Type': 'application/json;charset=utf-8' }
+    body = JSON.stringify(props)
+  }
+  const response = await fetch(path, { method, headers, body })
+  return response.json()
 }
