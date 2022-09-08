27d8add830069321cbf27b8323c07cb21cf87126
roll <eskarev@gmail.com>
2022-09-08 15:24:03 +0300

    Bootstrapped ErrorsView

--- src/components/Table/Layout.tsx
+++ src/components/Table/Layout.tsx
@@ -4,6 +4,7 @@ import Tab from '@mui/material/Tab'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 import DataView from './Views/Data'
+import ErrorsView from './Views/Errors'
 import MetadataView from './Views/Metadata'
 import QueryView from './Views/Query'
 import ReportView from './Views/Report'
@@ -21,8 +22,9 @@ export default function Editor() {
           <Tab label="Data" {...a11yProps(0)} />
           <Tab label="Metadata" {...a11yProps(1)} />
           <Tab label="Report" {...a11yProps(2)} />
-          <Tab label="Query" {...a11yProps(3)} />
-          <Tab label="Source" {...a11yProps(4)} />
+          <Tab label="Errors" {...a11yProps(3)} />
+          <Tab label="Query" {...a11yProps(4)} />
+          <Tab label="Source" {...a11yProps(5)} />
         </Tabs>
       </Box>
       <TabPanel value={value} index={0}>
@@ -35,9 +37,12 @@ export default function Editor() {
         <ReportView />
       </TabPanel>
       <TabPanel value={value} index={3}>
-        <QueryView />
+        <ErrorsView />
       </TabPanel>
       <TabPanel value={value} index={4}>
+        <QueryView />
+      </TabPanel>
+      <TabPanel value={value} index={5}>
         <SourceView />
       </TabPanel>
     </Box>
