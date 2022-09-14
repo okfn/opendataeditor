2fffc3c0ad41719afeb666d33453d62a4ff8e901
roll <eskarev@gmail.com>
2022-09-14 16:39:48 +0300

    Recovered SqlEditor

---
+++ src/components/Editors/Sql/Content.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import Box from '@mui/material/Box'
+
+export default function Content() {
+  return <Box>Sql</Box>
+}
---
+++ src/components/Editors/Sql/store.ts
@@ -0,0 +1,29 @@
+import * as React from 'react'
+import * as zustand from 'zustand'
+import create from 'zustand/vanilla'
+import { assert } from 'ts-essentials'
+import { Client } from '../../../client'
+import { LinksProps } from './Links'
+
+export interface State {
+  // Data
+
+  client: Client
+}
+
+export function createStore(props: LinksProps) {
+  return create<State>((_set, _get) => ({
+    // Data
+
+    client: props.client,
+  }))
+}
+
+export function useStore<R>(selector: (state: State) => R): R {
+  const store = React.useContext(StoreContext)
+  assert(store, 'store provider is required')
+  return zustand.useStore(store, selector)
+}
+
+const StoreContext = React.createContext<zustand.StoreApi<State> | null>(null)
+export const StoreProvider = StoreContext.Provider
