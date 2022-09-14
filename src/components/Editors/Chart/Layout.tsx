693f6c43f02f439d7e6bf0d3947e522ca75e885e
roll <eskarev@gmail.com>
2022-09-14 16:38:15 +0300

    Recovered ChartEditor

---
+++ src/components/Editors/Chart/Layout.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import Box from '@mui/material/Box'
+import { useTheme } from '@mui/material/styles'
+import Actions from './Actions'
+import Content from './Content'
+
+export default function Layout() {
+  const theme = useTheme()
+  const height = `calc(100vh - ${theme.spacing(8 + 6)})`
+  return (
+    <Box sx={{ height, display: 'flex', flexDirection: 'column' }}>
+      <Box>
+        <Content />
+      </Box>
+      <Box sx={{ marginTop: 'auto' }}>
+        <Actions />
+      </Box>
+    </Box>
+  )
+}
