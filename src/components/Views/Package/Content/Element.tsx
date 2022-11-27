220a8cde297d1f5d30a0e4ffe59c4f7ab9f41c96
shashi gharti <shashi.gharti@gmail.com>
2022-11-27 12:07:21 +0545

    Fixed type error

--- src/components/Views/Package/Content/Element.tsx
+++ src/components/Views/Package/Content/Element.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import { assert } from 'ts-essentials'
 import MenuItem from '@mui/material/MenuItem'
-import { useTheme } from '@mui/material/styles'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import Dialog from '@mui/material/Dialog'
