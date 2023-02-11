8152f8380c9fa5bc22ab9528ef70e730cedae357
roll <eskarev@gmail.com>
2023-02-11 15:57:20 +0000

    Added examplar field tree

--- src/components/Controllers/Sql/Panels/Config.tsx
+++ src/components/Controllers/Sql/Panels/Config.tsx
@@ -1,8 +1,30 @@
 import * as React from 'react'
 import Box from '@mui/material/Box'
 import MultilineField from '../../../Parts/Fields/MultilineField'
+import BaseTree from '../../../Parts/Trees/BaseTree'
 import Columns from '../../../Parts/Columns'
 import { useStore } from '../store'
+
+const EXAMPLE_TREE = [
+  {
+    name: 'Table 1',
+    path: 'table1',
+    type: 'table',
+    children: [
+      { name: 'Field 1', path: 'table1/field1', type: 'field', children: [] },
+      { name: 'Field 2', path: 'table1/field2', type: 'field', children: [] },
+    ],
+  },
+  {
+    name: 'Table 2',
+    path: 'table2',
+    type: 'table',
+    children: [
+      { name: 'Field 1', path: 'table2/field1', type: 'field', children: [] },
+      { name: 'Field 2', path: 'table2/field2', type: 'field', children: [] },
+    ],
+  },
+]
 
 export default function ConfigPanel() {
   const query = useStore((state) => state.query)
@@ -15,7 +37,9 @@ export default function ConfigPanel() {
         value={query}
         onChange={(query) => setQuery(query)}
       />
-      <Box sx={{ padding: 2 }}>field explorer</Box>
+      <Box sx={{ marginY: 2, border: 'solid 1px #ddd' }}>
+        <BaseTree tree={EXAMPLE_TREE} />
+      </Box>
     </Columns>
   )
 }
