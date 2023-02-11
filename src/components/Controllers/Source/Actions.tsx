d466b3603b70101d9acaf8150a92032701803c9b
roll <eskarev@gmail.com>
2023-02-11 09:26:53 +0000

    Updated Source actions

--- src/components/Controllers/Source/Actions.tsx
+++ src/components/Controllers/Source/Actions.tsx
@@ -1,30 +1,23 @@
 import * as React from 'react'
 import Box from '@mui/material/Box'
-import { useStore } from './store'
-import { Grid } from '@mui/material'
-import { useTheme } from '@mui/material/styles'
-import ExportButton from '../../Parts/Buttons/ExportButton'
+import ExportIcon from '@mui/icons-material/IosShare'
+import DefaultButton from '../../Parts/Buttons/DefaultButton'
+import Columns from '../../Parts/Columns'
 
 export default function Actions() {
-  const theme = useTheme()
-  const height = `calc(${theme.spacing(8)} - 1px)`
-  const isPreview = useStore((state) => state.isPreview)
-  const exportFormat = useStore((state) => state.exportFormat)
-  const setExportFormat = useStore((state) => state.setExportFormat)
-  const togglePreview = useStore((state) => state.togglePreview)
-  const exporter = useStore((state) => state.exporter)
+  // TODO: move styling to Layout?
+  // TODO: instead of 63px use proper calculation: theme.spacing(8) - 1px
   return (
-    <Box sx={{ lineHeight: height, borderTop: 1, borderColor: 'divider', paddingX: 2 }}>
-      <Grid item xs={4}>
-        <ExportButton
-          format={exportFormat}
-          options={['csv', 'xlsx']}
-          isPreview={isPreview}
-          onExport={exporter}
-          onPreview={togglePreview}
-          setFormat={setExportFormat}
-        />
-      </Grid>
+    <Box sx={{ borderTop: 'solid 1px #ddd', lineHeight: '63px', paddingX: 2 }}>
+      <Columns spacing={2}>
+        <Export />
+      </Columns>
     </Box>
+  )
+}
+
+function Export() {
+  return (
+    <DefaultButton icon={<ExportIcon fontSize="small" sx={{ mr: 1 }} />} label="Export" />
   )
 }
