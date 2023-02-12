69051dc32958562baeefafcfb166d46c916bfedd
roll <eskarev@gmail.com>
2023-02-12 12:28:11 +0000

    Added Chart editor

--- src/components/Controllers/Chart/Panels/Config.tsx
+++ src/components/Controllers/Chart/Panels/Config.tsx
@@ -1,18 +1,6 @@
 import * as React from 'react'
-import InputField from '../../../Parts/Fields/InputField'
-import VerticalTabs from '../../../Parts/VerticalTabs'
-import { useStore } from '../store'
+import Chart from '../../../Editors/Chart'
 
 export default function ConfigPanel() {
-  const axisX = useStore((state) => state.axisX)
-  const axisY = useStore((state) => state.axisY)
-  const setAxisX = useStore((state) => state.setAxisX)
-  const setAxisY = useStore((state) => state.setAxisY)
-  return (
-    <VerticalTabs labels={['Data', 'Axis (X)', 'Axis (Y)']}>
-      <InputField label="Select table/view: show schema/data preview" value="" />
-      <InputField label="Axis (X)" value={axisX} onChange={(axisX) => setAxisX(axisX)} />
-      <InputField label="Axis (Y)" value={axisY} onChange={(acisY) => setAxisY(acisY)} />
-    </VerticalTabs>
-  )
+  return <Chart />
 }
