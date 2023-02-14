11259391d419bd1d5e5618b901a55847bf2e7b61
Shashi Gharti <shashi.gharti@gmail.com>
2023-02-14 15:54:02 +0545

    Confirmation screen (#117)
    * Add confirmation dialog for delete
    
    * Cancel confirmation dialog
    
    * Remove confirmprovider from application level and add it to the Files controller
    
    * Fix button and title
    
    Change title based on folder/file deletion
    Change button size
    Change text
    
    * Capitalize "Delete Folder/File"
    
    * Minor dialog tweaks
    
    * Fixed typo
    
    ---------
    
    Co-authored-by: roll <eskarev@gmail.com>

--- src/components/Controllers/Files/Buttons/DeleteButton.tsx
+++ src/components/Controllers/Files/Buttons/DeleteButton.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react'
 import DeleteIcon from '@mui/icons-material/Delete'
 import DefaultButton from '../../../Parts/Buttons/DefaultButton'
-import { useStore } from '../store'
+import { selectors, useStore } from '../store'
+import { useConfirm } from 'material-ui-confirm'
 
 export default function DeleteButton() {
   const path = useStore((state) => state.path)
   const deleteFile = useStore((state) => state.deleteFile)
+  const isFolder = useStore(selectors.isFolder)
+  const confirm = useConfirm()
+  const type = isFolder ? 'Folder' : 'File'
   return (
     <DefaultButton
       label="Delete"
@@ -13,7 +17,25 @@ export default function DeleteButton() {
       disabled={!path}
       variant="text"
       color="warning"
-      onClick={() => deleteFile()}
+      onClick={() => {
+        confirm({
+          title: `Delete ${type}`,
+          confirmationText: 'Delete',
+          description: `You are deleting "${path}". Are you sure?`,
+          confirmationButtonProps: {
+            color: 'secondary',
+            variant: 'contained',
+            sx: { width: '50%' },
+          },
+          cancellationButtonProps: {
+            color: 'warning',
+            variant: 'contained',
+            sx: { width: '50%' },
+          },
+        }).then(() => {
+          deleteFile()
+        })
+      }}
     />
   )
 }
