a32fe6ff1c759af5bd1a632675fbc168b4c3e269
Edgar Zanella Alvarenga <e@vaz.io>
2023-02-24 12:46:39 +0100

    move IViewError interface to better place

--- src/interfaces/view.ts
+++ src/interfaces/view.ts
@@ -1,3 +1,13 @@
 export interface IView {
   query: string
 }
+
+export enum ViewErrorLocation {
+  Backend,
+  Frontend
+}
+
+export interface IViewError {
+  message: string
+  location: ViewErrorLocation
+}
