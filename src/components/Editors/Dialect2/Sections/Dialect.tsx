b7e74ab5c684777ff358836c155a488c0a6b4492
roll <eskarev@gmail.com>
2023-03-18 13:55:25 +0000

    Added dialect section

--- src/components/Editors/Dialect2/Sections/Dialect.tsx
+++ src/components/Editors/Dialect2/Sections/Dialect.tsx
@@ -1,87 +1,76 @@
 import * as React from 'react'
 import Box from '@mui/material/Box'
 import InputField from '../../../Parts/Fields/InputField'
-import MultiselectField from '../../../Parts/Fields/MultiselectField'
-import MultilineField from '../../../Parts/Fields/MultilineField'
+import YesNoField from '../../../Parts/Fields/YesNoField'
 import EditorSection from '../../../Parts/Editor/EditorSection'
 import Columns from '../../../Parts/Columns'
-import { useStore, selectors } from '../store'
+import * as settings from '../../../../settings'
+import { useStore } from '../store'
 
 export default function General() {
   const updateHelp = useStore((state) => state.updateHelp)
   return (
-    <EditorSection name="Schema" onHeadingClick={() => updateHelp('schema')}>
+    <EditorSection name="Dialect" onHeadingClick={() => updateHelp('dialect')}>
       <Columns spacing={2}>
         <Box>
-          <Title />
-          <Description />
+          <Header />
+          <HeaderRows />
         </Box>
         <Box>
-          <PrimaryKey />
-          <MissingValues />
+          <HeaderJoin />
+          <HeaderCase />
         </Box>
       </Columns>
     </EditorSection>
   )
 }
 
-function Title() {
-  const title = useStore((state) => state.schema.title)
+function Header() {
+  const header = useStore((state) => state.dialect.header)
   const updateHelp = useStore((state) => state.updateHelp)
-  const updateSchema = useStore((state) => state.updateSchema)
+  const updateDialect = useStore((state) => state.updateDialect)
+  return (
+    <YesNoField
+      label="Header"
+      value={header || settings.DEFAULT_HEADER}
+      onFocus={() => updateHelp('dialect/header')}
+      onChange={(header) => updateDialect({ header })}
+    />
+  )
+}
+
+function HeaderRows() {
+  const headerRows = useStore((state) => state.dialect.headerRows)
+  const updateDialect = useStore((state) => state.updateDialect)
   return (
     <InputField
-      label="Title"
-      value={title || ''}
-      onFocus={() => updateHelp('schema/title')}
-      onChange={(value) => updateSchema({ title: value || undefined })}
+      label="Header Rows"
+      value={headerRows}
+      onChange={(headerRows) => updateDialect({ headerRows })}
     />
   )
 }
 
-function Description() {
-  const description = useStore((state) => state.schema.description)
-  const updateHelp = useStore((state) => state.updateHelp)
-  const updateSchema = useStore((state) => state.updateSchema)
-  return (
-    <MultilineField
-      label="Description"
-      value={description || ''}
-      onFocus={() => updateHelp('schema/description')}
-      onChange={(value) => updateSchema({ description: value || undefined })}
-    />
-  )
-}
-
-function PrimaryKey() {
-  const fieldNames = useStore(selectors.fieldNames)
-  const primaryKey = useStore((state) => state.schema.primaryKey)
-  const updateHelp = useStore((state) => state.updateHelp)
-  const updateSchema = useStore((state) => state.updateSchema)
-  return (
-    <MultiselectField
-      label="Primary Key"
-      value={primaryKey || []}
-      options={fieldNames}
-      onFocus={() => updateHelp('schema/primaryKey')}
-      onChange={(value) => updateSchema({ primaryKey: value || undefined })}
-    />
-  )
-}
-
-// TODO: support empty strings
-function MissingValues() {
-  const missingValues = useStore((state) => state.schema.missingValues)
-  const updateHelp = useStore((state) => state.updateHelp)
-  const updateSchema = useStore((state) => state.updateSchema)
+function HeaderJoin() {
+  const headerJoin = useStore((state) => state.dialect.headerJoin)
+  const updateDialect = useStore((state) => state.updateDialect)
   return (
     <InputField
-      label="Missing Values"
-      value={(missingValues || []).join(',')}
-      onFocus={() => updateHelp('schema/missingValues')}
-      onChange={(value) =>
-        updateSchema({ missingValues: value ? value.split(',') : undefined })
-      }
+      label="Header Join"
+      value={headerJoin}
+      onChange={(headerJoin) => updateDialect({ headerJoin })}
+    />
+  )
+}
+
+function HeaderCase() {
+  const headerCase = useStore((state) => state.dialect.headerCase)
+  const updateDialect = useStore((state) => state.updateDialect)
+  return (
+    <YesNoField
+      label="Header Case"
+      value={headerCase || settings.DEFAULT_HEADER_CASE}
+      onChange={(headerCase) => updateDialect({ headerCase })}
     />
   )
 }
--- src/components/Editors/Dialect2/store.ts
+++ src/components/Editors/Dialect2/store.ts
@@ -4,182 +4,44 @@ import { assert } from 'ts-essentials'
 import noop from 'lodash/noop'
 import cloneDeep from 'lodash/cloneDeep'
 import { createStore } from 'zustand/vanilla'
-import { createSelector } from 'reselect'
-import { ISchema, IField, IForeignKey, IHelpItem } from '../../../interfaces'
-import { SchemaProps } from './Schema'
+import { IDialect, IHelpItem } from '../../../interfaces'
+import { DialectProps } from './Dialect'
 import * as helpers from '../../../helpers'
 import help from './help.yaml'
 
-const INITIAL_SCHEMA: ISchema = { fields: [] }
-const DEFAULT_HELP_ITEM = helpers.readHelpItem(help, 'schema')!
-
-interface ISectionState {
-  query?: string
-  index?: number
-  isGrid?: boolean
-  isExtras?: boolean
-}
+const INITIAL_DIALECT: IDialect = {}
+const DEFAULT_HELP_ITEM = helpers.readHelpItem(help, 'dialect')!
 
 interface State {
-  schema: ISchema
-  onChange: (schema: ISchema) => void
-  onFieldSelected: (name: string) => void
+  dialect: IDialect
+  onChange: (dialect: IDialect) => void
   helpItem: IHelpItem
   updateHelp: (path: string) => void
 
-  // Schema
+  // Dialect
 
-  updateSchema: (patch: Partial<ISchema>) => void
-
-  // Fields
-
-  fieldState: ISectionState
-  updateFieldState: (patch: Partial<ISectionState>) => void
-  updateField: (patch: Partial<IField>) => void
-  removeField: () => void
-  addField: () => void
-
-  // Foreign Keys
-
-  foreignKeyState: ISectionState
-  updateForeignKeyState: (patch: Partial<ISectionState>) => void
-  updateForeignKey: (patch: Partial<IForeignKey>) => void
-  removeForeignKey: () => void
-  addForeignKey: () => void
+  updateDialect: (patch: Partial<IDialect>) => void
 }
 
-export function makeStore(props: SchemaProps) {
+export function makeStore(props: DialectProps) {
   return createStore<State>((set, get) => ({
-    schema: cloneDeep(props.schema || INITIAL_SCHEMA),
+    dialect: cloneDeep(props.dialect || INITIAL_DIALECT),
     onChange: props.onChange || noop,
-    onFieldSelected: props.onFieldSelected || noop,
     helpItem: DEFAULT_HELP_ITEM,
     updateHelp: (path) => {
       const helpItem = helpers.readHelpItem(help, path) || DEFAULT_HELP_ITEM
       set({ helpItem })
     },
 
-    // Schema
+    // Dialect
 
-    updateSchema: (patch) => {
-      let { schema, onChange } = get()
-      schema = { ...schema, ...patch }
-      onChange(schema)
-      set({ schema })
-    },
-
-    // Fields
-
-    fieldState: {},
-    updateFieldState: (patch) => {
-      const { fieldState } = get()
-      set({ fieldState: { ...fieldState, ...patch } })
-    },
-    updateField: (patch) => {
-      const { schema, updateSchema } = get()
-      const { index, field } = selectors.field(get())
-      const fields = schema.fields
-      fields[index] = { ...field, ...patch }
-      updateSchema({ fields })
-    },
-    removeField: () => {
-      const { schema, updateSchema, updateFieldState } = get()
-      const { index } = selectors.field(get())
-      const fields = [...schema.fields]
-      fields.splice(index, 1)
-      updateFieldState({ index: undefined, isExtras: false })
-      updateSchema({ fields })
-    },
-    // TODO: scroll to newly created field
-    addField: () => {
-      const { schema, updateSchema } = get()
-      const fields = [...schema.fields]
-      // TODO: deduplicate
-      const name = `field${fields.length}`
-      fields.push({ name, type: 'string' })
-      updateSchema({ fields })
-    },
-
-    // Foreign Keys
-
-    foreignKeyState: {},
-    updateForeignKeyState: (patch) => {
-      const { foreignKeyState } = get()
-      set({ foreignKeyState: { ...foreignKeyState, ...patch } })
-    },
-    updateForeignKey: (patch) => {
-      const { schema, updateSchema } = get()
-      const { index, foreignKey } = selectors.foreignKey(get())
-      const foreignKeys = schema.foreignKeys!
-      foreignKeys[index] = { ...foreignKey, ...patch }
-      updateSchema({ foreignKeys })
-    },
-    removeForeignKey: () => {
-      const { schema, updateSchema, updateForeignKeyState } = get()
-      const { index } = selectors.foreignKey(get())
-      const foreignKeys = [...(schema.foreignKeys || [])]
-      foreignKeys.splice(index, 1)
-      updateForeignKeyState({ index: undefined, isExtras: false })
-      updateSchema({ foreignKeys })
-    },
-    // TODO: scroll to newly created field
-    addForeignKey: () => {
-      const { schema, updateSchema } = get()
-      const foreignKeys = [...(schema.foreignKeys || [])]
-      // TODO: catch no fields
-      const name = schema.fields[0].name
-      foreignKeys.push({
-        fields: [name],
-        reference: { fields: [name], resource: '' },
-      })
-      updateSchema({ foreignKeys })
+    updateDialect: (patch) => {
+      let { dialect, onChange } = get()
+      dialect = { ...dialect, ...patch }
+      onChange(dialect)
+      set({ dialect })
     },
   }))
-}
-
-export const select = createSelector
-export const selectors = {
-  // Fields
-
-  field: (state: State) => {
-    const index = state.fieldState.index!
-    const field = state.schema.fields[index]!
-    return { index, field }
-  },
-  fieldNames: (state: State) => {
-    return state.schema.fields.map((field) => field.name)
-  },
-  foundFieldItems: (state: State) => {
-    const items = []
-    const query = state.fieldState.query
-    for (const [index, field] of state.schema.fields.entries()) {
-      if (query && !field.name.includes(query)) continue
-      items.push({ index, field })
-    }
-    return items
-  },
-
-  // Foreign Keys
-
-  foreignKey: (state: State) => {
-    const index = state.foreignKeyState.index!
-    const foreignKeys = state.schema.foreignKeys!
-    const foreignKey = foreignKeys[index]!
-    return { index, foreignKey }
-  },
-  foreignKeyNames: (state: State) => {
-    return (state.schema.foreignKeys || []).map((fk) => fk.fields.join(','))
-  },
-  foundForeignKeyItems: (state: State) => {
-    const items = []
-    const query = state.foreignKeyState.query
-    for (const [index, fk] of (state.schema.foreignKeys || []).entries()) {
-      const name = fk.fields.join(',')
-      if (query && !name.includes(query)) continue
-      items.push({ index, foreignKey: fk })
-    }
-    return items
-  },
 }
 
 export function useStore<R>(selector: (state: State) => R): R {
