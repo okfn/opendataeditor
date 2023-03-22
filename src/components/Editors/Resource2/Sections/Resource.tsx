9aa5ef2cd142bed03ac7d14c5c6d4f1666c3cf9c
roll <eskarev@gmail.com>
2023-03-22 12:18:59 +0000

    Added more fields to resource

--- src/components/Editors/Resource2/Sections/Resource.tsx
+++ src/components/Editors/Resource2/Sections/Resource.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import Box from '@mui/material/Box'
 import InputField from '../../../Parts/Fields/InputField'
+import SelectField from '../../../Parts/Fields/SelectField'
 import MultilineField from '../../../Parts/Fields/MultilineField'
 import EditorSection from '../../../Parts/Editor/EditorSection'
 import Columns from '../../../Parts/Columns'
@@ -14,10 +15,14 @@ export default function Resource() {
         <Box>
           <Name />
           <Type />
-        </Box>
-        <Box>
           <Title />
           <Description />
+        </Box>
+        <Box>
+          <Path />
+          <Scheme />
+          <Format />
+          <Encoding />
         </Box>
       </Columns>
     </EditorSection>
@@ -40,7 +45,15 @@ function Name() {
 
 function Type() {
   const type = useStore((state) => state.descriptor.type)
-  return <InputField disabled label="Type" value={type} />
+  const updateDescriptor = useStore((state) => state.updateDescriptor)
+  return (
+    <SelectField
+      label="Type"
+      value={type}
+      options={['file', 'text', 'json', 'table']}
+      onChange={(value) => updateDescriptor({ type: value || 'file' })}
+    />
+  )
 }
 
 function Title() {
@@ -67,6 +80,62 @@ function Description() {
       value={description || ''}
       onFocus={() => updateHelp('resource/description')}
       onChange={(value) => updateDescriptor({ description: value || undefined })}
+    />
+  )
+}
+
+function Path() {
+  const path = useStore((state) => state.descriptor.path)
+  const updateHelp = useStore((state) => state.updateHelp)
+  const updateDescriptor = useStore((state) => state.updateDescriptor)
+  return (
+    <InputField
+      label="Path"
+      value={path}
+      onFocus={() => updateHelp('resource/path')}
+      onChange={(path) => updateDescriptor({ path })}
+    />
+  )
+}
+
+function Scheme() {
+  const scheme = useStore((state) => state.descriptor.scheme)
+  const updateHelp = useStore((state) => state.updateHelp)
+  const updateDescriptor = useStore((state) => state.updateDescriptor)
+  return (
+    <InputField
+      label="Scheme"
+      value={scheme}
+      onFocus={() => updateHelp('resource/scheme')}
+      onChange={(scheme) => updateDescriptor({ scheme })}
+    />
+  )
+}
+
+function Format() {
+  const format = useStore((state) => state.descriptor.format)
+  const updateHelp = useStore((state) => state.updateHelp)
+  const updateDescriptor = useStore((state) => state.updateDescriptor)
+  return (
+    <InputField
+      label="Format"
+      value={format}
+      onFocus={() => updateHelp('resource/format')}
+      onChange={(format) => updateDescriptor({ format })}
+    />
+  )
+}
+
+function Encoding() {
+  const encoding = useStore((state) => state.descriptor.encoding)
+  const updateHelp = useStore((state) => state.updateHelp)
+  const updateDescriptor = useStore((state) => state.updateDescriptor)
+  return (
+    <InputField
+      label="Encoding"
+      value={encoding}
+      onFocus={() => updateHelp('resource/encoding')}
+      onChange={(encoding) => updateDescriptor({ encoding })}
     />
   )
 }
