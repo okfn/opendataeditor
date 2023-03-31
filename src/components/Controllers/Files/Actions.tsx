040c0c6feb131ad535fffded89787cba0aad0d61
roll <eskarev@gmail.com>
2023-03-31 18:25:08 +0100

    Rebased Files on ActionsBar

--- src/components/Controllers/Files/Actions.tsx
+++ src/components/Controllers/Files/Actions.tsx
@@ -1,22 +1,18 @@
 import * as React from 'react'
-import Box from '@mui/material/Box'
 import Columns from '../../Parts/Columns'
+import ActionsBar from '../../Parts/Bars/Actions'
 import CreateButton from './Buttons/CreateButton'
 import DeleteButton from './Buttons/DeleteButton'
 import ManageButton from './Buttons/ManageButton'
 
 export default function Actions() {
-  // TODO: fix geometry
-  // TODO: instead of 63px use proper calculation: theme.spacing(8) - 1px
   return (
-    <Box
-      sx={{ borderTop: 'solid 1px #ddd', lineHeight: '58px', backgroundColor: '#fafafa' }}
-    >
+    <ActionsBar>
       <Columns spacing={2}>
         <CreateButton />
         <ManageButton />
         <DeleteButton />
       </Columns>
-    </Box>
+    </ActionsBar>
   )
 }
