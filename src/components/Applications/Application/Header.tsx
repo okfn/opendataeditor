eadf41963dd5dcbdc327be586afcbe05c29af492
roll <eskarev@gmail.com>
2023-04-01 17:05:13 +0100

    Bootstrapped Status controller

--- src/components/Applications/Application/Header.tsx
+++ src/components/Applications/Application/Header.tsx
@@ -1,18 +1,16 @@
 import * as React from 'react'
 import { useTheme } from '@mui/material/styles'
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
 import Chip from '@mui/material/Chip'
-import Typography from '@mui/material/Typography'
-import { alpha, styled } from '@mui/material/styles'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Button from '@mui/material/Button'
-import InputIcon from '@mui/icons-material/Input'
+import Typography from '@mui/material/Typography'
 import SettingsIcon from '@mui/icons-material/Settings'
 import GithubIcon from '@mui/icons-material/GitHub'
 import HelpIcon from '@mui/icons-material/Help'
-import InputBase from '@mui/material/InputBase'
-import Grid from '@mui/material/Grid'
-import Box from '@mui/material/Box'
+import Status from '../../Controllers/Status'
 import { useStore } from './store'
 
 // TODO: rebase from props to state
@@ -42,17 +40,7 @@ export default function Header() {
               </Typography>
             </Grid>
             <Grid item xs={5} lg={7}>
-              <Search>
-                <SearchIconWrapper>
-                  <InputIcon />
-                </SearchIconWrapper>
-                <StyledInputBase
-                  placeholder="Data management application for browser and desktop"
-                  inputProps={{ 'aria-label': 'search' }}
-                  readOnly
-                  value={file ? file.path : ''}
-                />
-              </Search>
+              <Status file={file} />
             </Grid>
             <Grid item xs={3} lg={2}>
               <Grid container justifyContent="flex-end">
@@ -83,43 +71,3 @@ export default function Header() {
     </Box>
   )
 }
-
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(1),
-    width: 'auto',
-  },
-}))
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}))
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  width: '100%',
-  '& ::file-selector-button': {
-    display: 'none',
-  },
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-  },
-}))
