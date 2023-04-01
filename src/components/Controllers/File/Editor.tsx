c22d3312a97310cf2bd5853248c390f41603053f
roll <eskarev@gmail.com>
2023-04-01 14:23:19 +0100

    Added NonSupportedEditor for File

---
+++ src/components/Controllers/File/Editor.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react'
+import Box from '@mui/material/Box'
+import { useStore } from './store'
+import * as helpers from '../../../helpers'
+
+export default function Editor() {
+  const format = useStore((state) => state.file.record?.resource.format)
+  if (['png', 'jpg'].includes(format || '')) return <ImageEditor />
+  return <NonSupportedEditor />
+}
+
+function ImageEditor() {
+  const format = useStore((state) => state.file.record?.resource.format)
+  const original = useStore((state) => state.original)
+  if (!format) return null
+  if (!original) return null
+  const text = helpers.bytesToBase64(original)
+  return (
+    <Box sx={{ padding: 2 }}>
+      <img src={`data:image/${format};base64,${text}`} />
+    </Box>
+  )
+}
+
+function NonSupportedEditor() {
+  const format = useStore((state) => state.file.record?.resource.format)
+  return (
+    <Box
+      sx={{
+        height: '100%',
+        width: '100%',
+        backgroundColor: '#fafafa',
+        padding: 2,
+        color: '#777',
+        fontFamily: 'Monospace',
+      }}
+    >
+      This file type does not have a supported data view ({format})
+    </Box>
+  )
+}
--- src/components/Controllers/File/Layout.tsx
+++ src/components/Controllers/File/Layout.tsx
@@ -4,19 +4,19 @@ import { useTheme } from '@mui/material/styles'
 import ScrollBox from '../../Parts/ScrollBox'
 import MetadataPanel from './Panels/Metadata'
 import Actions from './Actions'
-import Content from './Content'
+import Editor from './Editor'
 import Dialog from './Dialog'
 import Menu from './Menu'
 import { useStore } from './store'
 
 export default function Layout() {
   const theme = useTheme()
-  const file = useStore((state) => state.file)
   const panel = useStore((state) => state.panel)
-  const loadContent = useStore((state) => state.loadContent)
   const height = `calc(100vh - ${theme.spacing(8)})`
   const panelHeight = panel ? 48 : 0
   const contentHeight = `calc(100vh - ${theme.spacing(8 + 8 + panelHeight)})`
+  const file = useStore((state) => state.file)
+  const loadContent = useStore((state) => state.loadContent)
   React.useEffect(() => {
     loadContent().catch(console.error)
   }, [file])
@@ -26,7 +26,7 @@ export default function Layout() {
       <Box sx={{ height, display: 'flex', flexDirection: 'column' }}>
         <Menu />
         <ScrollBox height={contentHeight}>
-          <Content />
+          <Editor />
         </ScrollBox>
         <Box
           hidden={!panel}
