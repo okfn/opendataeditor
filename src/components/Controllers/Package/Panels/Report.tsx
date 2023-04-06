f7530e28a126708e5c4d3dd3865d35ac58597af0
roll <eskarev@gmail.com>
2023-04-06 15:43:33 +0100

    Show report for package

--- src/components/Controllers/Package/Panels/Report.tsx
+++ src/components/Controllers/Package/Panels/Report.tsx
@@ -1,16 +1,31 @@
 import * as React from 'react'
 import Box from '@mui/material/Box'
-// import Report from '../../../Editors/Report'
+import Report from '../../../Editors/Report'
+import { IReport } from '../../../../interfaces'
 import { useStore } from '../store'
 
-// TODO: fix
 export default function ReportPanel() {
+  const file = useStore((state) => state.file)
   const report = useStore((state) => state.file.record?.report)
   if (!report) return null
-  return <Box sx={{ padding: 2 }}>report</Box>
-  // return (
-  // <Box>
-  // <Report report={report} />
-  // </Box>
-  // )
+  // TODO: for now there is not real metadata validation so we just hack Report to show valid
+  const stubReport: IReport = {
+    ...report,
+    tasks: [
+      {
+        valid: true,
+        name: 'name',
+        type: 'json',
+        place: file.path,
+        labels: [],
+        stats: { errors: 0, warnings: 0, seconds: 0 },
+        errors: [],
+      },
+    ],
+  }
+  return (
+    <Box sx={{ paddingX: 2 }}>
+      <Report report={stubReport} />
+    </Box>
+  )
 }
