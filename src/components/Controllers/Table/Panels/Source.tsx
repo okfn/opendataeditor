5fb33390500ac6b9f465c5eafda0076ebbfe66ea
roll <eskarev@gmail.com>
2023-04-16 19:48:43 +0100

    Rebased table/source on monaco

--- src/components/Controllers/Table/Panels/Source.tsx
+++ src/components/Controllers/Table/Panels/Source.tsx
@@ -1,14 +1,21 @@
 import * as React from 'react'
+import { useTheme } from '@mui/material/styles'
 import Box from '@mui/material/Box'
-import Code from '../../../Parts/Code'
+import MonacoEditor from '../../../Parts/Monaco/Editor'
 import { useStore } from '../store'
 
 export default function SourcePanel() {
+  const theme = useTheme()
   const source = useStore((state) => state.source)
   if (!source) return null
   return (
-    <Box sx={{ paddingY: 2, overflowY: 'auto' }}>
-      <Code source={source} />
+    <Box>
+      <MonacoEditor
+        value={source}
+        language="json"
+        height={theme.spacing(47)}
+        options={{ readOnly: true }}
+      />
     </Box>
   )
 }
