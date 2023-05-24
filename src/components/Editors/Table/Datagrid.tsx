d1c85bffb36781d049f3052922131eb0f9d40286
roll <eskarev@gmail.com>
2023-05-24 09:02:28 +0100

    Simplified datagrid

--- src/components/Editors/Table/Datagrid.tsx
+++ src/components/Editors/Table/Datagrid.tsx
@@ -20,46 +20,6 @@ export default function Datagrid() {
     updateState({ editing: true })
   }
 
-  const handleKeyDown = (event: any) => {
-    if (!editing) return
-    const grid = gridRef?.current
-    if (!grid) return
-    const cell = grid.computedActiveCell
-    if (!cell) return
-
-    // Start editing
-    let [rowIndex, colIndex] = cell
-    if (event.key === ' ' || event.key === 'Enter') {
-      const column = grid.getColumnBy(colIndex)
-      if (grid.startEdit && column.name) {
-        grid.startEdit({ columnId: column.name, rowIndex })
-      }
-      event.preventDefault()
-      return
-    }
-
-    // Navigate cells
-    if (event.key !== 'Tab') return
-    event.preventDefault()
-    event.stopPropagation()
-    const direction = event.shiftKey ? -1 : 1
-    const columns = grid.visibleColumns
-    const rowCount = grid.count
-    colIndex += direction
-    if (colIndex === -1) {
-      colIndex = columns.length - 1
-      rowIndex -= 1
-    }
-    if (colIndex === columns.length) {
-      rowIndex += 1
-      colIndex = 0
-    }
-    if (rowIndex < 0 || rowIndex === rowCount) {
-      return
-    }
-    grid.setActiveCell([rowIndex, colIndex])
-  }
-
   const handleEditComplete = (context: any) => {
     if (!editing) return
 
@@ -67,9 +27,7 @@ export default function Datagrid() {
     const rowNumber = context.rowId
     const fieldName = context.columnId
     let value = context.value
-    if (context.cellProps.type === 'number') {
-      value = parseInt(value)
-    }
+    if (context.cellProps.type === 'number') value = parseInt(value)
     if (onUpdate) onUpdate(rowNumber, fieldName, value)
   }
 
@@ -96,10 +54,9 @@ export default function Datagrid() {
       onReady={(gridRef) => updateState({ gridRef })}
       columns={columns}
       dataSource={loader as any}
-      onKeyDown={handleKeyDown}
       onEditStart={handleEditStart}
-      onEditStop={handleEditStop}
       onEditComplete={handleEditComplete}
+      onEditStop={handleEditStop}
       style={{ height: '100%', border: 'none' }}
     />
   )
