e321618a10fedc264d747c06da94a8514afcf3a2
roll <eskarev@gmail.com>
2023-06-01 17:12:27 +0100

    Updated record

--- src/types/record.ts
+++ src/types/record.ts
@@ -1,10 +1,8 @@
-import { IStats } from './stats'
 import { IResource } from './resource'
 
 export interface IRecord {
   name: string
   type: string
   path: string
-  stats: IStats
   resource: IResource
 }
