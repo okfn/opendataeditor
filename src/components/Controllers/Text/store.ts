f02d4935013eab388acbb59e2ef7d86ac1bba164
roll <eskarev@gmail.com>
2023-06-10 14:43:53 +0100

    Fixed article/script

--- src/components/Controllers/Text/store.ts
+++ src/components/Controllers/Text/store.ts
@@ -128,9 +128,8 @@ export function makeStore(props: TextProps) {
     render: throttle(async () => {
       const { record, client, modifiedText } = get()
       if (!record) return
-      if (!modifiedText) return
       if (record.type === 'article') {
-        const { text } = await client.articleRender({ text: modifiedText })
+        const { text } = await client.articleRender({ text: modifiedText || '' })
         set({ renderedText: text })
       }
     }, 1000),
