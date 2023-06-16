cedfe961a3ad516f885c37bc05baf675626ff861
roll <eskarev@gmail.com>
2023-06-16 15:16:04 +0100

    Removed tooltips from browser

--- src/components/Applications/Application/Buttons/Create.tsx
+++ src/components/Applications/Application/Buttons/Create.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import AddIcon from '@mui/icons-material/AddBox'
 import SourceIcon from '@mui/icons-material/Source'
@@ -9,26 +8,21 @@ import IconButton from '../../../Parts/Buttons/Icon'
 import AddLink from '@mui/icons-material/AddLink'
 import DriveFolderUploadRounded from '@mui/icons-material/DriveFolderUploadRounded'
 import UploadFileRounded from '@mui/icons-material/UploadFileRounded'
-import LightTooltip from '../../../Parts/Tooltips/Light'
 import { useStore } from '../store'
 
 export default function CreateButton() {
   return (
-    <LightTooltip title="Upload or create new">
-      <Box>
-        <DropdownButton
-          label="Create"
-          variant="text"
-          icon={<AddIcon fontSize="small" sx={{ mr: 1 }} />}
-        >
-          <UploadFile />
-          <UploadLink />
-          <UploadFolder />
-          <CreateFolder />
-          <CreatePackage />
-        </DropdownButton>
-      </Box>
-    </LightTooltip>
+    <DropdownButton
+      label="Create"
+      variant="text"
+      icon={<AddIcon fontSize="small" sx={{ mr: 1 }} />}
+    >
+      <UploadFile />
+      <UploadLink />
+      <UploadFolder />
+      <CreateFolder />
+      <CreatePackage />
+    </DropdownButton>
   )
 }
 
