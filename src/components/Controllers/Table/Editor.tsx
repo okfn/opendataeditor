bdfc9e9d0499fa3e3f9228bd5ee79a2bd93ea8e4
roll <eskarev@gmail.com>
2023-06-21 13:52:13 +0100

    Moved Table to Parts

--- src/components/Controllers/Table/Editor.tsx
+++ src/components/Controllers/Table/Editor.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import TableEditor from '../../Editors/Table'
+import TableEditor from '../../Parts/Table'
 import { useStore } from './store'
 
 export default function Editor() {
