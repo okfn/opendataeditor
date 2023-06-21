6b44e22405357e90976587e191d035be9f79fbc4
roll <eskarev@gmail.com>
2023-06-21 15:57:39 +0100

    Added metadata panel to base

--- src/components/Controllers/Table/Panels/Metadata.tsx
+++ src/components/Controllers/Table/Panels/Metadata.tsx
@@ -1,14 +1,12 @@
 import * as React from 'react'
-import Resource from '../../../Editors/Resource'
+import MetadataPanel from '../../Base/Panels/Metadata'
 import { useStore } from '../store'
 
-export default function MetadataPanel() {
+export default function Metadata() {
   const resource = useStore((state) => state.resource)
   const updateState = useStore((state) => state.updateState)
-  if (!resource) return null
   return (
-    <Resource
-      shallow={resource.type !== 'table'}
+    <MetadataPanel
       resource={resource}
       onChange={(resource) => updateState({ resource })}
       onFieldSelected={(name) => updateState({ selection: { columnName: name } })}
