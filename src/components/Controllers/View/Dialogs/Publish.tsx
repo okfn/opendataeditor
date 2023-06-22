f4918d0455464e2c22754c4de9bfb5b61817fd42
roll <eskarev@gmail.com>
2023-06-22 13:52:24 +0100

    Support view publishing

---
+++ src/components/Controllers/View/Dialogs/Publish.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import PublishDialog from '../../Base/Dialogs/Publish'
+import { useStore } from '../store'
+
+export default function Publish() {
+  const publish = useStore((state) => state.publish)
+  const updateState = useStore((state) => state.updateState)
+  return (
+    <PublishDialog
+      onPublish={publish}
+      onClose={() => updateState({ dialog: undefined })}
+    />
+  )
+}
