1aeeafb4e7a095ff32d99d1e00fa8b7fb46e0066
roll <eskarev@gmail.com>
2023-06-29 16:41:47 +0100

    Implemeted "Index All"

--- src/components/Application/Dialogs/IndexFiles.tsx
+++ src/components/Application/Dialogs/IndexFiles.tsx
@@ -5,6 +5,9 @@ import LinearSensor from '../../Parts/Sensors/Linear'
 import { useStore, selectors } from '../store'
 
 export default function IndexFilesDialog() {
+  const [progress, setProgress] = React.useState(0)
+  const client = useStore((state) => state.client)
+  const loadFiles = useStore((state) => state.loadFiles)
   const updateState = useStore((state) => state.updateState)
   const notIndexedFiles = useStore(selectors.notIndexedFiles)
   return (
@@ -13,13 +16,19 @@ export default function IndexFilesDialog() {
       title="Index Files"
       label="Index"
       Icon={DeleteIcon}
+      disabled={!!progress}
       description={`Index not indexed files (${notIndexedFiles.length}). It might take some time`}
       onCancel={() => updateState({ dialog: undefined })}
       onConfirm={async () => {
+        for (const [index, file] of notIndexedFiles.entries()) {
+          await client.fileIndex({ path: file.path })
+          setProgress((100 * (index + 1)) / notIndexedFiles.length)
+        }
         updateState({ dialog: undefined })
+        await loadFiles()
       }}
     >
-      <LinearSensor value={0} labeled />
+      {progress > 0 && progress < 100 && <LinearSensor value={progress} labeled />}
     </ConfirmDialog>
   )
 }
