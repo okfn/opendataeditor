95d10e9b17b64b774fe0dc0a0f42b9984c6d8e3d
roll <eskarev@gmail.com>
2023-06-30 16:25:16 +0100

    Fit chart to the screen size

--- src/components/Controllers/Chart/Viewer.tsx
+++ src/components/Controllers/Chart/Viewer.tsx
@@ -1,18 +1,23 @@
 import * as React from 'react'
-import { VegaLite } from 'react-vega'
 import Box from '@mui/material/Box'
+import { VegaLite } from 'react-vega'
 import { useStore } from './store'
+
+// To understand how we make it fit the screen read:
+// https://github.com/vega/react-vega/issues/85
 
 export default function Viewer() {
   const rendered = useStore((state) => state.rendered)
   if (!rendered) return null
   return (
-    <Box sx={{ height: '100%', padding: 2 }}>
-      <VegaLite
-        spec={rendered as any}
-        width={rendered.width || 600}
-        height={rendered.height || 400}
-      />
+    <Box
+      sx={{
+        padding: 2,
+        height: '100%',
+        '& .vega-embed': { width: '100%', height: '100%' },
+      }}
+    >
+      <VegaLite spec={{ ...rendered, width: 'container', height: 'container' } as any} />
     </Box>
   )
 }
