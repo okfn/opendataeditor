54dd6c1e4a63ab3c6d1bad5177e2f788c289d5ab
roll <eskarev@gmail.com>
2023-07-01 09:41:26 +0100

    Removed legacy

--- legacy/Table/interfaces.ts
+++
@@ -1,8 +0,0 @@
-import { IError, IDict } from '../../../interfaces'
-
-export interface IErrorIndex {
-  header: IDict<IError[]>
-  label: IDict<IError[]>
-  row: IDict<IError[]>
-  cell: IDict<IError[]>
-}
--- legacy/libraries/vega-presets/groups/barCharts.ts
+++
@@ -1,8 +0,0 @@
-import * as group from '../group'
-import simpleBarChart from '../presets/simpleBarChart'
-
-export default new group.Group({
-  type: 'bar',
-  title: 'Bar Charts',
-  presets: [simpleBarChart],
-})
