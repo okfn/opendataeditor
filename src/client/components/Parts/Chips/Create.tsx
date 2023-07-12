662774a43ff597866c633c6b968c6de38cf05474
roll <eskarev@gmail.com>
2023-07-12 16:15:08 +0100

    Added shortcut for create

--- src/client/components/Parts/Chips/Create.tsx
+++ src/client/components/Parts/Chips/Create.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import Chip from '@mui/material/Chip'
 import AutoFixHighIcon from '@mui/icons-material/AutoFixHigh'
 import LightTooltip from '../Tooltips/Light'
+import { useKeyPress } from 'ahooks'
 
 // TODO: create BaseChip / move concrete to appliction?
 
@@ -10,8 +11,12 @@ export interface CreateChipProps {
 }
 
 export default function CreateChip(props: CreateChipProps) {
+  useKeyPress(['alt.c'], (event) => {
+    event.preventDefault()
+    props.onClick()
+  })
   return (
-    <LightTooltip title="Create chart, dataset, and more">
+    <LightTooltip title="Create chart, dataset, and more [Alt+C]">
       <Chip
         onClick={props.onClick}
         color="primary"
