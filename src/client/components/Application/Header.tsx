4305d1ec02a322073659983c2da1a78f1ec2d443
roll <eskarev@gmail.com>
2023-09-08 11:17:23 +0100

    Fixed tests

--- src/client/components/Application/Header.tsx
+++ src/client/components/Application/Header.tsx
@@ -28,7 +28,14 @@ export default function Header() {
               <LightTooltip title="Show the project page">
                 <Typography
                   variant="h5"
-                  sx={{ paddingLeft: 4, fontSize: '28px', ml: 2, mt: '4px', cursor: 'pointer', whiteSpace: 'nowrap' }}
+                  sx={{
+                    paddingLeft: 4,
+                    fontSize: '28px',
+                    ml: 2,
+                    mt: '4px',
+                    cursor: 'pointer',
+                    whiteSpace: 'nowrap',
+                  }}
                   onClick={() => closeFile()}
                 >
                   <strong>
