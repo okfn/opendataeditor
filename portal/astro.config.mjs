641c715cae52e541d44a041375153e83c391d8cb
roll <eskarev@gmail.com>
2023-09-08 14:24:06 +0100

    Improved homepage

--- portal/astro.config.mjs
+++ portal/astro.config.mjs
@@ -14,6 +14,7 @@ export default defineConfig({
       social: {
         github: 'https://github.com/frictionlessdata/open-data-editor',
       },
+      customCss: ['/src/assets/styles.css'],
       sidebar: [
         {
           label: 'Guides',
