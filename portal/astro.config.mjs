8d884c04adf3c2606ad7f4692f79ef9499e535d5
roll <roll@users.noreply.github.com>
2023-09-08 17:11:05 +0100

    Improved documentation website (#247)
    * Added portal
    
    * Updated logo
    
    * Improved homepage
    
    * Improved homepage
    
    * Fixed scrollbar
    
    * Updated docs
    
    * Fixed build

---
+++ portal/astro.config.mjs
@@ -0,0 +1,30 @@
+import {defineConfig} from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: 'Open Data Editor',
+      logo: {
+        light: '/src/assets/logo-light-orange.png',
+        dark: '/src/assets/logo-dark-orange.png',
+        replacesTitle: true,
+      },
+      social: {
+        github: 'https://github.com/frictionlessdata/open-data-editor',
+      },
+      customCss: ['/src/assets/styles.css'],
+      sidebar: [
+        {
+          label: 'Documentation',
+          autogenerate: {directory: 'documentation'},
+        },
+        {
+          label: 'Contributing',
+          autogenerate: {directory: 'contributing'},
+        },
+      ],
+    }),
+  ],
+});
---
+++ portal/src/content/docs/documentation/getting-started.md
@@ -0,0 +1,22 @@
+---
+title: Getting Started
+sidebar:
+  order: 1
+---
+
+:::caution
+The currently published version is out-dated. Please wait for the v1 release that will be distributed as a desktop application. You you want to try it now, we reccomend running a [development version](../../contributing/development/).
+:::
+
+
+To install a beta version of the Frictionless Application ensure that you have Python 3.8+ available and run this command:
+
+```bash
+$ pip3 install frictionless[application]==5.13.1
+```
+
+To start the application:
+
+```bash
+$ frictionless application <folder>
+```
