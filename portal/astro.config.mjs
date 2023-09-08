e8db716d8c35bb7b6d687f7bb6a0f1f32ee31178
roll <eskarev@gmail.com>
2023-09-08 15:19:17 +0100

    Improved homepage

--- portal/astro.config.mjs
+++ portal/astro.config.mjs
@@ -7,14 +7,14 @@ export default defineConfig({
     starlight({
       title: 'Open Data Editor',
       logo: {
-        light: '/src/assets/logo-light.png',
-        dark: '/src/assets/logo-dark.png',
+        light: '/src/assets/logo-light-orange.png',
+        dark: '/src/assets/logo-dark-orange.png',
         replacesTitle: true,
       },
       social: {
         github: 'https://github.com/frictionlessdata/open-data-editor',
       },
-      customCss: ['/src/assets/styles.css'],
+      customCss: ['@fontsource/roboto', '/src/assets/styles.css'],
       sidebar: [
         {
           label: 'Guides',
