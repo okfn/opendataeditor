3b738a03fec284f4e3dd9f0dc28d363cc8f57415
roll <eskarev@gmail.com>
2023-09-20 13:36:28 +0100

    Implemented AppImage build

--- desktop/index.ts
+++ desktop/index.ts
@@ -41,7 +41,7 @@ function createWindow(): void {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron')
+  electronApp.setAppUserModelId('org.opendataeditor')
 
   // Default open or close DevTools by F12 in development
   // and ignore CommandOrControl + R in production.
