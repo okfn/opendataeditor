4b9d2f861504cc2737d2c03774560b26900f77f5
roll <eskarev@gmail.com>
2023-09-21 10:53:02 +0100

    Fixed ignored folders

--- server/settings.py
+++ server/settings.py
@@ -4,58 +4,12 @@ APPNAME = "odet"
 ARTIFACTS_IDENTIFIER = "_artifacts"
 BUFFER_SIZE = 1000
 DEFAULT_HTTP_PORT = 4040
-
 IGNORED_FOLDERS = [
+    ".venv",
+    ".python",
     "node_modules",
-    "logs" "*.logs",
-    ".pyc",
-    ".idea/",
-    ".vscode/",
-    "*.sublime*",
-    ".DS_STORE",
-    "npm-debug.log*",
-    "package-lock.json",
-    "/.cache",
-    "*.sqlite",
-    # Byte-compiled
-    ".pytest_cache/",
-    ".ruff_cache/",
-    "__pycache__/",
-    # Unit test / coverage
-    ".coverage",
-    ".coverage.*",
-    "coverage.xml",
-    "*.py[cod]",
-    ".pytest_cache/",
-    ".tox/",
-    ".nox/",
-    "cover/",
-    "*.whl",
-    # C
-    "*.so"
-    # Distribution
-    "bin/",
-    "build/",
-    "develop-eggs/",
-    "dist/",
-    "downloads/",
-    "eggs/",
-    ".eggs/",
-    "lib/",
-    "lib64/",
-    "parts",
-    "sdist/",
-    "var/",
-    "wheels/",
-    "share/python-wheels/",
-    "*.egg-info/",
-    ".installed.cfg",
-    "*.egg",
-    "MANIFEST"
-    # Jupyter
-    ".ipynb_checkpoints",
-    # mypy
-    ".mypy_cache/",
-    ".dmypy.json",
-    "dmypy.json",
+    ".pytest_cache",
+    ".ruff_cache",
+    "__pycache__",
+    ".odet",
 ]
