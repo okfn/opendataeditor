e3b609d42c4b0b589c0f123fc60dd4f7aac5c37b
roll <eskarev@gmail.com>
2023-09-22 11:14:35 +0100

    Improved logging

--- desktop/system.ts
+++ desktop/system.ts
@@ -5,11 +5,6 @@ const execFilePromise = util.promisify(cp.execFile)
 
 export async function execFile(path: string, args: string[]) {
   log.info('[execFile]', path, args.join(' '))
-
-  const { stdout, stderr } = await execFilePromise(path, args)
-
-  if (stdout) log.info('[execFile]', 'stdout', stdout)
-  if (stderr) log.error('[execFile]', 'stderr', stderr)
-
+  const { stdout } = await execFilePromise(path, args)
   return stdout
 }
