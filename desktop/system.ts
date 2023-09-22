b71bb93effcaaa9669e03a8a75138534954dad3e
roll <eskarev@gmail.com>
2023-09-22 16:08:27 +0100

    Added project/check endpoint

--- desktop/system.ts
+++ desktop/system.ts
@@ -12,8 +12,8 @@ export async function execFile(path: string, args: string[], cwd?: string) {
 export async function spawnFile(path: string, args: string[], cwd?: string) {
   log.info('[spawnFile]', { path, args, cwd })
   const proc = cp.spawn(path, args, { cwd })
-  proc.stdout.on('data', (data) => log.info(data.toString()))
-  proc.stderr.on('data', (data) => log.error(data.toString()))
+  proc.stdout.on('data', (data) => log.info(data.toString().trim()))
+  proc.stderr.on('data', (data) => log.error(data.toString().trim()))
   proc.on('close', (code) => {
     log.info('[spawnFile]', { message: `child process exited with code ${code}` })
   })
