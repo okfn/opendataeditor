26bfefecf76df38abaf5a05ba99961229d8900af
roll <eskarev@gmail.com>
2023-09-23 16:08:36 +0100

    Added open project button

--- client/components/Application/Menu.tsx
+++ client/components/Application/Menu.tsx
@@ -1,25 +1,41 @@
+import Button from '@mui/material/Button'
 import InputAdornment from '@mui/material/InputAdornment'
 import SelectField from '../Parts/Fields/Select'
+import Columns from '../Parts/Grids/Columns'
 import * as menu from '../Parts/Bars/Menu'
 import { useStore } from './store'
 
 export default function Menu() {
   const name = useStore((state) => state.config?.project.name || 'current')
+  // @ts-ignore
+  const selectFolder = window?.opendataeditor?.selectFolder
   return (
     <menu.MenuBar fullWidth>
-      <SelectField
-        disabled
-        margin="none"
-        value={name}
-        options={[name]}
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start" disableTypography>
-              Project:
-            </InputAdornment>
-          ),
-        }}
-      />
+      <Columns layout={[9, 3]} spacing={1}>
+        <SelectField
+          disabled={!selectFolder}
+          margin="none"
+          value={name}
+          options={[name]}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start" disableTypography>
+                Project:
+              </InputAdornment>
+            ),
+          }}
+        />
+        <Button
+          color="inherit"
+          disabled={!selectFolder}
+          variant="outlined"
+          fullWidth
+          onClick={selectFolder ? selectFolder : undefined}
+          sx={{ height: '100%', borderColor: '#bbb !important' }}
+        >
+          Open
+        </Button>
+      </Columns>
     </menu.MenuBar>
   )
 }
