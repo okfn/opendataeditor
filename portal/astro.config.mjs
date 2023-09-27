042a539cd22a7ad28c9016cd335e5864bf63ddcd
roll <eskarev@gmail.com>
2023-09-27 16:59:28 +0100

    Updated logos

--- portal/astro.config.mjs
+++ portal/astro.config.mjs
@@ -1,5 +1,5 @@
-import {defineConfig} from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
 
 // https://astro.build/config
 export default defineConfig({
@@ -7,24 +7,67 @@ export default defineConfig({
     starlight({
       title: 'Open Data Editor',
       logo: {
-        light: '/src/assets/logo-light-orange.png',
-        dark: '/src/assets/logo-dark-orange.png',
+        light: '/src/assets/logo-light.svg',
+        dark: '/src/assets/logo-dark.svg',
         replacesTitle: true,
       },
       social: {
         github: 'https://github.com/frictionlessdata/open-data-editor',
       },
+      favicon: 'favicon.ico',
       customCss: ['/src/assets/styles.css'],
       sidebar: [
         {
           label: 'Documentation',
-          autogenerate: {directory: 'documentation'},
+          autogenerate: { directory: 'documentation' },
         },
         {
           label: 'Contributing',
-          autogenerate: {directory: 'contributing'},
-        }
+          autogenerate: { directory: 'contributing' },
+        },
+      ],
+      head: [
+        {
+          tag: 'link',
+          attrs: {
+            rel: 'icon',
+            href: '/favicon-16x16.png',
+            sizes: '16x16',
+          },
+        },
+        {
+          tag: 'link',
+          attrs: {
+            rel: 'icon',
+            href: '/favicon-32x32.png',
+            sizes: '32x32',
+          },
+        },
+        {
+          tag: 'link',
+          attrs: {
+            rel: 'apple-touch-icon',
+            href: '/apple-touch-icon.png',
+            sizes: '180x180',
+          },
+        },
+        {
+          tag: 'link',
+          attrs: {
+            rel: 'icon',
+            href: '/android-chrome-192x192.png',
+            sizes: '192x192',
+          },
+        },
+        {
+          tag: 'link',
+          attrs: {
+            rel: 'icon',
+            href: '/android-chrome-512x512.png',
+            sizes: '512x512',
+          },
+        },
       ],
     }),
   ],
-});
+})
