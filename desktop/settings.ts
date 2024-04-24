46d23b5fec279912292b18551ba82c4c425ead29
pdelboca <patriciodelboca@gmail.com>
2024-04-24 05:57:56 -0300

    Rename APP_PROJECTS to APP_TMP

--- desktop/settings.ts
+++ desktop/settings.ts
@@ -16,7 +16,8 @@ export const APP_USER_MODEL_ID = 'org.opendataeditor'
 export const APP_HOME = join(HOME, `.${APP_NAME}`)
 export const APP_RUNNER = join(APP_HOME, 'runner')
 export const APP_PYTHON = join(APP_HOME, 'python')
-export const APP_PROJECTS = join(APP_HOME, 'projects')
+// APP_TMP will be the folder to upload files the first time the user opens the Application
+export const APP_TMP = join(APP_HOME, 'tmp')
 
 export const PYTHON_SOURCE = join(APP_RUNNER, WIN ? 'python.exe' : 'bin/python3')
 export const PYTHON_TARGET = join(APP_PYTHON, WIN ? 'Scripts\\python.exe' : 'bin/python3')
