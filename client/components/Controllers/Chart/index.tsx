5e46522616641c1a43d70fad3ac9e0798b6cb8d9
pdelboca <patriciodelboca@gmail.com>
2024-05-08 13:32:50 +0200

    Remove unnecessary ThemeProvider calls.

--- client/components/Controllers/Chart/index.tsx
+++ client/components/Controllers/Chart/index.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react'
-import { ThemeProvider } from '@mui/material/styles'
 import { StoreProvider, makeStore } from './store'
-import * as themes from '../../../themes'
 import { ControllerProps } from '../../Controllers/Base'
 import Layout from './Layout'
 
@@ -10,10 +8,8 @@ export interface ChartProps extends ControllerProps {}
 export default function Chart(props: ChartProps) {
   const store = React.useMemo(() => makeStore(props), Object.values(props))
   return (
-    <ThemeProvider theme={themes.DEFAULT}>
-      <StoreProvider value={store}>
-        <Layout />
-      </StoreProvider>
-    </ThemeProvider>
+    <StoreProvider value={store}>
+      <Layout />
+    </StoreProvider>
   )
 }
--- client/components/Controllers/View/index.tsx
+++ client/components/Controllers/View/index.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react'
-import { ThemeProvider } from '@mui/material/styles'
 import { StoreProvider, makeStore } from './store'
-import * as themes from '../../../themes'
 import { ControllerProps } from '../../Controllers/Base'
 import Layout from './Layout'
 
@@ -10,10 +8,8 @@ export interface ViewProps extends ControllerProps {}
 export default function View(props: ViewProps) {
   const store = React.useMemo(() => makeStore(props), Object.values(props))
   return (
-    <ThemeProvider theme={themes.DEFAULT}>
-      <StoreProvider value={store}>
-        <Layout />
-      </StoreProvider>
-    </ThemeProvider>
+    <StoreProvider value={store}>
+      <Layout />
+    </StoreProvider>
   )
 }
