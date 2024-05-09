faf5182a6776034a4ac88ee09e88391e97a8f806
Patricio Del Boca <patriciodelboca@gmail.com>
2024-05-09 12:32:37 +0200

    Remove unnecessary ThemeProvider calls. (#376)

--- client/components/Controllers/Metadata/index.tsx
+++ client/components/Controllers/Metadata/index.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react'
 import { StoreProvider, makeStore } from './store'
-import { ThemeProvider } from '@mui/material/styles'
-import * as themes from '../../../themes'
 import { ControllerProps } from '../../Controllers/Base'
 import Layout from './Layout'
 
@@ -10,10 +8,8 @@ export interface MetadataProps extends ControllerProps {}
 export default function Metadata(props: MetadataProps) {
   const store = React.useMemo(() => makeStore(props), Object.values(props))
   return (
-    <ThemeProvider theme={themes.DEFAULT}>
-      <StoreProvider value={store}>
-        <Layout />
-      </StoreProvider>
-    </ThemeProvider>
+    <StoreProvider value={store}>
+      <Layout />
+    </StoreProvider>
   )
 }
