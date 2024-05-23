b04c86e8a2fcb462466f04af2d56b88cd2453dd4
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2024-05-23 10:09:49 +0200

    Move url validation logic inside AddRemoteFile input

--- client/components/Parts/Dialogs/Input.tsx
+++ client/components/Parts/Dialogs/Input.tsx
@@ -2,31 +2,22 @@ import * as React from 'react'
 import InputAdornment from '@mui/material/InputAdornment'
 import TextField from '@mui/material/TextField'
 import ConfirmDialog, { ConfirmDialogProps } from './Confirm'
-import * as helpers from '../../../helpers'
 
 export interface InputDialogProps extends Omit<ConfirmDialogProps, 'onConfirm'> {
   value?: string
   prefix?: string
   placholder?: string
   spellcheck?: boolean
-  onConfirm?: (value: string) => void
+  onConfirm?: (value: string) => void,
+  textFieldError? : boolean,
+  errorHelperText?: string
 }
 
 export default function InputDialog(props: InputDialogProps) {
-  const { value: initValue, prefix, placholder, spellcheck, onConfirm, ...rest } = props
+  const { value: initValue, prefix, placholder, spellcheck, onConfirm, textFieldError, errorHelperText, ...rest } = props
   const [value, setValue] = React.useState(initValue || '')
-  const [textFieldError, setTextFieldError] = React.useState(false)
-  const [errorHelperText, setErrorHelperText] = React.useState('')
-  const handleConfirm = () => {
-    if(value !== '' && helpers.isUrlValid(value)){
-      setTextFieldError(false)
-      return onConfirm && onConfirm(value)
-    } else {
-      setTextFieldError(true)
-      if(value === '') { setErrorHelperText('Enter an URL') }
-      else if(!helpers.isUrlValid(value)) { setErrorHelperText('Enter a valid URL') }
-    }
-  }
+
+  const handleConfirm = () => onConfirm && onConfirm(value)
   return (
     <ConfirmDialog {...rest} onConfirm={handleConfirm}>
       <TextField
