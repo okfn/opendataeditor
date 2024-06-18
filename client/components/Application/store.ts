6e5ca7fd8a350937ba8413e8c6c1ab9a0e9d4b9e
roll <eskarev@gmail.com>
2024-06-18 08:18:43 +0100

    Catch errors in the application component

--- client/components/Application/store.ts
+++ client/components/Application/store.ts
@@ -37,12 +37,12 @@ export interface State {
 
   // Config
 
-  loadConfig: () => Promise<void>
+  loadConfig: (throwError?: boolean) => Promise<void>
   saveConfig: (config: types.IConfig) => Promise<void>
 
   // File
 
-  loadFiles: () => Promise<void>
+  loadFiles: (throwError?: boolean) => Promise<void>
   addFiles: (files: FileList) => Promise<void>
   fetchFile: (url: string) => Promise<void>
   createFile: (path: string, prompt?: string) => Promise<void>
@@ -96,8 +96,8 @@ export function makeStore(props: ApplicationProps) {
       const delaySeconds = 1
       while (!ready) {
         try {
-          await loadConfig()
-          await loadFiles()
+          await loadConfig(true)
+          await loadFiles(true)
           ready = true
         } catch (error) {
           attempt += 1
@@ -112,8 +112,14 @@ export function makeStore(props: ApplicationProps) {
 
       // Setup project sync polling
       setInterval(async () => {
-        const { files } = await client.projectSync({})
-        updateState({ files })
+        const result = await client.projectSync({})
+
+        // Here we ignore errors for now and just update the files on success
+        if (result instanceof client.Error) {
+          return
+        }
+
+        updateState({ files: result.files })
       }, settings.PROJECT_SYNC_INTERVAL_MILLIS)
 
       updateState({ dialog: undefined })
@@ -148,7 +154,12 @@ export function makeStore(props: ApplicationProps) {
       const { client, updateState, closeFile, loadConfig, loadFiles } = get()
       closeFile()
       updateState({ loading: true })
-      await client.projectOpen({ fullpath })
+      const result = await client.projectOpen({ fullpath })
+
+      if (result instanceof client.Error) {
+        return updateState({ error: result })
+      }
+
       await loadConfig()
       await loadFiles()
       updateState({ loading: false })
@@ -156,76 +167,133 @@ export function makeStore(props: ApplicationProps) {
 
     // Config
 
-    loadConfig: async () => {
+    loadConfig: async (throwError?: boolean) => {
       const { client, updateState } = get()
-      const { config } = await client.configRead()
-      updateState({ config })
+      const result = await client.configRead()
+
+      if (result instanceof client.Error) {
+        if (throwError) throw new Error(result.detail)
+        return updateState({ error: result })
+      }
+
+      updateState({ config: result.config })
     },
 
     saveConfig: async (config) => {
-      const { client, loadConfig } = get()
-      await client.configWrite({ config })
+      const { client, loadConfig, updateState } = get()
+      const result = await client.configWrite({ config })
+
+      if (result instanceof client.Error) {
+        return updateState({ error: result })
+      }
+
       await loadConfig()
     },
 
     // File
 
-    loadFiles: async () => {
+    loadFiles: async (throwError?: boolean) => {
       const { client, updateState } = get()
-      const { files } = await client.fileList()
-      updateState({ files })
+      const result = await client.fileList()
+
+      if (result instanceof client.Error) {
+        if (throwError) throw new Error(result.detail)
+        return updateState({ error: result })
+      }
+
+      updateState({ files: result.files })
     },
     addFiles: async (files) => {
-      const { client, onFileCreate } = get()
+      const { client, onFileCreate, updateState } = get()
       const folder = selectors.folderPath(get())
       const paths: string[] = []
       for (const file of files) {
         const path = file.webkitRelativePath || undefined
         const result = await client.fileCreate({ file, path, folder, deduplicate: true })
+
+        if (result instanceof client.Error) {
+          return updateState({ error: result })
+        }
+
         paths.push(result.path)
       }
       onFileCreate(paths)
     },
     fetchFile: async (url) => {
-      const { client, onFileCreate } = get()
+      const { client, onFileCreate, updateState } = get()
       const folder = selectors.folderPath(get())
-      const { path } = await client.fileFetch({ url, folder, deduplicate: true })
-      onFileCreate([path])
+      const result = await client.fileFetch({ url, folder, deduplicate: true })
+
+      if (result instanceof client.Error) {
+        return updateState({ error: result })
+      }
+
+      onFileCreate([result.path])
     },
     createFile: async (path, prompt) => {
-      const { client, onFileCreate } = get()
+      const { client, onFileCreate, updateState } = get()
       if (prompt) {
         const text = ''
         const result = await client.textCreate({ path, text, prompt, deduplicate: true })
+
+        if (result instanceof client.Error) {
+          return updateState({ error: result })
+        }
+
         onFileCreate([result.path])
       } else {
         const file = new File([new Blob()], path)
         const result = await client.fileCreate({ path, file, deduplicate: true })
+
+        if (result instanceof client.Error) {
+          return updateState({ error: result })
+        }
+
         onFileCreate([result.path])
       }
     },
     adjustFile: async (name, type) => {
-      const { path, client, closeFile, loadFiles, selectFile } = get()
+      const { path, client, closeFile, loadFiles, selectFile, updateState } = get()
       if (!path) return
-      await client.filePatch({ path, name, type })
+      const result = await client.filePatch({ path, name, type })
+
+      if (result instanceof client.Error) {
+        return updateState({ error: result })
+      }
+
       set({ path: undefined })
       closeFile()
       await loadFiles()
       await selectFile(path)
     },
     copyFile: async (path, toPath) => {
-      const { client, onFileCreate } = get()
+      const { client, onFileCreate, updateState } = get()
       const result = await client.fileCopy({ path, toPath, deduplicate: true })
+
+      if (result instanceof client.Error) {
+        return updateState({ error: result })
+      }
+
       onFileCreate([result.path])
     },
     deleteFile: async (path) => {
-      const { client, onFileDelete } = get()
-      await client.fileDelete({ path })
+      const { client, onFileDelete, updateState } = get()
+      const result = await client.fileDelete({ path })
+
+      if (result instanceof client.Error) {
+        return updateState({ error: result })
+      }
+
       onFileDelete(path)
     },
     moveFile: async (path, toPath) => {
-      const { client, onFileCreate } = get()
+      const { client, onFileCreate, updateState } = get()
       const result = await client.fileMove({ path, toPath, deduplicate: true })
+
+      if (result instanceof client.Error) {
+        return updateState({ error: result })
+      }
+
       onFileCreate([result.path])
     },
     locateFile: async (path) => {
@@ -244,12 +312,17 @@ export function makeStore(props: ApplicationProps) {
       await openFile(newPath)
     },
     openFile: async (path) => {
-      const { client, loadFiles, fileEvent } = get()
+      const { client, loadFiles, fileEvent, updateState } = get()
       set({ record: undefined, measure: undefined })
       set({ indexing: true })
-      const { record, measure } = await client.fileIndex({ path })
+      const result = await client.fileIndex({ path })
+
+      if (result instanceof client.Error) {
+        return updateState({ error: result })
+      }
+
       await loadFiles()
-      set({ indexing: false, record, measure })
+      set({ indexing: false, record: result.record, measure: result.measure })
       if (!fileEvent) set({ fileEvent: { type: 'open', paths: [path] } })
       await delay(500)
       set({ fileEvent: undefined })
@@ -261,105 +334,142 @@ export function makeStore(props: ApplicationProps) {
     // Folder
 
     copyFolder: async (path, toPath) => {
-      const { client, onFileCreate } = get()
+      const { client, onFileCreate, updateState } = get()
       const result = await client.folderCopy({ path, toPath, deduplicate: true })
+
+      if (result instanceof client.Error) {
+        return updateState({ error: result })
+      }
+
       onFileCreate([result.path])
     },
     createFolder: async (path) => {
-      const { client, onFileCreate } = get()
+      const { client, onFileCreate, updateState } = get()
       const result = await client.folderCreate({ path, deduplicate: true })
+
+      if (result instanceof client.Error) {
+        return updateState({ error: result })
+      }
+
       onFileCreate([result.path])
     },
     deleteFolder: async (path) => {
-      const { client, onFileDelete } = get()
-      await client.folderDelete({ path })
+      const { client, onFileDelete, updateState } = get()
+      const result = await client.folderDelete({ path })
+
+      if (result instanceof client.Error) {
+        return updateState({ error: result })
+      }
+
       onFileDelete(path)
     },
     moveFolder: async (path, toPath) => {
-      const { client, onFileCreate } = get()
+      const { client, onFileCreate, updateState } = get()
       const result = await client.folderMove({ path, toPath, deduplicate: true })
+
+      if (result instanceof client.Error) {
+        return updateState({ error: result })
+      }
+
       onFileCreate([result.path])
     },
 
     // Others
 
     createArticle: async (path, prompt) => {
-      const { client, onFileCreate } = get()
-      const result = await client.articleCreate({
-        path,
-        prompt,
-        deduplicate: true,
-      })
+      const { client, onFileCreate, updateState } = get()
+      const result = await client.articleCreate({ path, prompt, deduplicate: true })
+
+      if (result instanceof client.Error) {
+        return updateState({ error: result })
+      }
+
       onFileCreate([result.path])
     },
     createChart: async (path, prompt) => {
-      const { client, onFileCreate } = get()
-      const result = await client.chartCreate({
-        path,
-        prompt,
-        deduplicate: true,
-      })
+      const { client, onFileCreate, updateState } = get()
+      const result = await client.chartCreate({ path, prompt, deduplicate: true })
+
+      if (result instanceof client.Error) {
+        return updateState({ error: result })
+      }
+
       onFileCreate([result.path])
     },
     createImage: async (path, prompt) => {
-      const { client, onFileCreate } = get()
-      const result = await client.imageCreate({
-        path,
-        prompt,
-        deduplicate: true,
-      })
+      const { client, onFileCreate, updateState } = get()
+      const result = await client.imageCreate({ path, prompt, deduplicate: true })
+
+      if (result instanceof client.Error) {
+        return updateState({ error: result })
+      }
+
       onFileCreate([result.path])
     },
     createMap: async (path, prompt) => {
-      const { client, onFileCreate } = get()
-      const result = await client.mapCreate({
-        path,
-        prompt,
-        deduplicate: true,
-      })
+      const { client, onFileCreate, updateState } = get()
+      const result = await client.mapCreate({ path, prompt, deduplicate: true })
+
+      if (result instanceof client.Error) {
+        return updateState({ error: result })
+      }
+
       onFileCreate([result.path])
     },
     createPackage: async (path, prompt) => {
-      const { client, onFileCreate } = get()
-      const result = await client.packageCreate({
-        path,
-        prompt,
-        deduplicate: true,
-      })
+      const { client, onFileCreate, updateState } = get()
+      const result = await client.packageCreate({ path, prompt, deduplicate: true })
+
+      if (result instanceof client.Error) {
+        return updateState({ error: result })
+      }
+
       onFileCreate([result.path])
     },
     fetchPackage: async (url) => {
-      const { client, onFileCreate } = get()
+      const { client, onFileCreate, updateState } = get()
       const folder = selectors.folderPath(get())
-      const { path } = await client.packageFetch({ url, folder, deduplicate: true })
-      onFileCreate([path])
+      const result = await client.packageFetch({ url, folder, deduplicate: true })
+
+      if (result instanceof client.Error) {
+        return updateState({ error: result })
+      }
+
+      onFileCreate([result.path])
     },
     createScript: async (path, prompt) => {
-      const { client, onFileCreate } = get()
-      const result = await client.scriptCreate({
-        path,
-        prompt,
-        deduplicate: true,
-      })
+      const { client, onFileCreate, updateState } = get()
+      const result = await client.scriptCreate({ path, prompt, deduplicate: true })
+
+      if (result instanceof client.Error) {
+        return updateState({ error: result })
+      }
+
       onFileCreate([result.path])
     },
     createTable: async (path, prompt) => {
-      const { client, onFileCreate } = get()
+      const { client, onFileCreate, updateState } = get()
       const result = await client.textCreate({
         path,
         text: '',
         prompt,
         deduplicate: true,
       })
+
+      if (result instanceof client.Error) {
+        return updateState({ error: result })
+      }
+
       onFileCreate([result.path])
     },
     createView: async (path, prompt) => {
-      const { client, onFileCreate } = get()
-      const result = await client.viewCreate({
-        path,
-        prompt,
-        deduplicate: true,
-      })
+      const { client, onFileCreate, updateState } = get()
+      const result = await client.viewCreate({ path, prompt, deduplicate: true })
+
+      if (result instanceof client.Error) {
+        return updateState({ error: result })
+      }
+
       onFileCreate([result.path])
     },
   }))
