8fd35c0141b5ba32dbf803e0fccb1a2f3dae6b2c
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2024-06-24 21:23:27 +0200

    Making some progress in generating letters after Z

--- client/components/Editors/Table/groups.tsx
+++ client/components/Editors/Table/groups.tsx
@@ -5,8 +5,19 @@ interface Group {
 
 export function createGroups(letterAmount: number): Array<Group> {
   // Generate an array with letters from the latin alphabet
-  const alpha = Array.from(Array(letterAmount)).map((_e, i) => i + 65)
-  const alphabet = alpha.map((x) => String.fromCharCode(x))
+  const letterACode = 'A'.charCodeAt(0)
+  const letterZCode = 'Z'.charCodeAt(0)
+  const alpha = Array.from(Array(letterAmount)).map((_e, i) => i + letterACode)
+  const alphabet = alpha.map((x) => {
+    if (x <= letterZCode) return String.fromCharCode(x)
+    else {
+      return (
+        String.fromCharCode(letterACode + Math.floor((x - letterACode) / 26)) +
+        String.fromCharCode(letterACode + (x % 26))
+      )
+    }
+  })
+  console.log('alphabet', alphabet)
 
   const groups: Array<Group> = []
 
