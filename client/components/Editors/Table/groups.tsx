14b249c3d20988316786464f017e73979500a428
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2024-06-24 22:17:39 +0200

    Add new logic

--- client/components/Editors/Table/groups.tsx
+++ client/components/Editors/Table/groups.tsx
@@ -3,25 +3,32 @@ interface Group {
   header: string
 }
 
-export function createGroups(letterAmount: number): Array<Group> {
+export function createGroups(columnsAmount: number): Array<Group> {
   // Generate an array with letters from the latin alphabet
   const letterACode = 'A'.charCodeAt(0)
-  const letterZCode = 'Z'.charCodeAt(0)
-  const alpha = Array.from(Array(letterAmount)).map((_e, i) => i + letterACode)
-  const alphabet = alpha.map((x) => {
-    if (x <= letterZCode) return String.fromCharCode(x)
-    else {
-      return (
-        String.fromCharCode(letterACode + Math.floor((x - letterACode) / 26)) +
-        String.fromCharCode(letterACode + (x % 26))
+  const totalLettersAlphabet = 26
+
+  const columnLabelsArray = []
+  let index1, index2
+
+  for (index1 = 0; index1 < totalLettersAlphabet; index1++) {
+    columnLabelsArray.push(String.fromCharCode(letterACode + index1))
+  }
+  for (index1 = 0; index1 < totalLettersAlphabet; index1++) {
+    for (index2 = 0; index2 < totalLettersAlphabet; index2++) {
+      if (columnLabelsArray.length === columnsAmount) {
+        break
+      }
+      columnLabelsArray.push(
+        String.fromCharCode(letterACode + index1) +
+          String.fromCharCode(letterACode + index2)
       )
     }
-  })
-  console.log('alphabet', alphabet)
+  }
 
   const groups: Array<Group> = []
 
-  alphabet.forEach((letter) => {
+  columnLabelsArray.forEach((letter) => {
     const letterEntry = {
       name: letter,
       header: letter,
