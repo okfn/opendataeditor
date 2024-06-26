2f8c873ea465348ff04a137ef0d932f50e761f33
pdelboca <patriciodelboca@gmail.com>
2024-06-26 09:52:37 +0200

    Add docstrings to build_python and rename folder

--- build.py
+++ build.py
@@ -15,16 +15,18 @@ def build_assets():
 
 
 def build_python():
-    # Define the source and destination paths
+    """ Zip the Python virtual environment created by hatch.
+
+    In order to be able to run a FastAPI server, we ship with ODE a
+    zipped virtualenvironment with all the dependencies installed.
+    """
     source_folder = os.path.expanduser('.python/opendataeditor')
-    build_runner_folder = 'build/runner'
+    build_runner_folder = 'build/python'
     zip_filename = 'pythonvenv'
     zip_filepath = os.path.join(build_runner_folder, zip_filename + '.zip')
 
-    # Ensure the build/runner folder exists
     os.makedirs(build_runner_folder, exist_ok=True)
 
-    # Create a zip file with
     with zipfile.ZipFile(zip_filepath, 'w', zipfile.ZIP_DEFLATED) as zipf:
         for root, dirs, files in os.walk(source_folder):
             for file in files:
@@ -32,11 +34,10 @@ def build_python():
                 arcname = os.path.relpath(file_path, start=source_folder)
                 zipf.write(file_path, arcname=arcname)
 
-    # Verify if the zip file was created successfully
     if os.path.isfile(zip_filepath):
-        print(f'Successfully zipped and copied to {zip_filepath}')
+        print(f'[build_python] Virtual environment zipped ({zip_filepath})')
     else:
-        print('Failed to zip the source folder.')
+        print('[build_python] Failed to zip the virtual environment.')
 
 
 def build_server():
