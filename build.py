b453ac6d8463b39f6dc6a8791fa08f54a6469b3e
pdelboca <patriciodelboca@gmail.com>
2024-06-27 12:31:45 +0200

    Build server using pyinstaller

--- build.py
+++ build.py
@@ -1,9 +1,6 @@
 import os
 import shutil
-import sys
-import tarfile
-
-import fsspec
+import PyInstaller.__main__
 
 
 def build_assets():
@@ -17,47 +14,16 @@ def build_assets():
     print(f"[assets] Copied '{source}' to '{target}'")
 
 
-def build_runner():
-    cache = ".cache"
-    target = "build/runner"
-
-    os.makedirs(cache, exist_ok=True)
-    shutil.rmtree(target, ignore_errors=True)
-
-    datemark = "20230826"
-    basepath = "https://github.com/indygreg/python-build-standalone/releases/download"
-    filetype = "x86_64-unknown-linux-gnu-install_only"
-    if sys.platform == "darwin":
-        filetype = "x86_64-apple-darwin-install_only"
-    if sys.platform == "win32":
-        filetype = "x86_64-pc-windows-msvc-shared-install_only"
-    filename = f"cpython-3.10.13+{datemark}-{filetype}.tar.gz"
-
-    if not os.path.exists(f"{cache}/{filename}"):
-        local = fsspec.filesystem("file")
-        remote = fsspec.filesystem("http")
-        with local.open(f"{cache}/{filename}", "wb") as file_to:
-            with remote.open(f"{basepath}/{datemark}/{filename}", "rb") as file_from:
-                file_to.write(file_from.read())
-
-    with tarfile.open(f"{cache}/{filename}", "r:gz") as tar:
-        tar.extractall(cache)
-        shutil.move(f"{cache}/python", target)
-
-    print(f"[runner] Copied 'runner' to '{target}'")
-
-
 def build_server():
-    source = "server"
-    target = "build/server"
-
-    shutil.rmtree(target, ignore_errors=True)
-    shutil.copytree(source, target)
-
-    print(f"[server] Copied '{source}' to '{target}'")
+    PyInstaller.__main__.run([
+        'server/__main__.py',
+        '--onefile',
+        '--name', 'server',
+        '--collect-all', 'frictionless',
+        '--distpath', 'build/server'
+    ])
 
 
 if __name__ == "__main__":
     build_assets()
-    build_runner()
     build_server()
