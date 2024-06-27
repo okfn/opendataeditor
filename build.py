6782a6563c5f380a19b5b07ea1620bff69f40359
pdelboca <patriciodelboca@gmail.com>
2024-06-27 14:18:18 +0200

    Add build comments

--- build.py
+++ build.py
@@ -7,21 +7,24 @@ def build_assets():
     source = "desktop/assets"
     target = "build"
 
+    print(f"[assets] Copying '{source}' to '{target}'")
+
     os.makedirs(target, exist_ok=True)
     for name in os.listdir(source):
         shutil.copy(f"{source}/{name}", target)
 
-    print(f"[assets] Copied '{source}' to '{target}'")
-
 
 def build_server():
     """Build an executable file for the FastAPI server."""
+
+    print("[server] Creating executable file for FastAPI server")
     PyInstaller.__main__.run([
         'server/__main__.py',
         '--onefile',
         '--name', 'server',
         '--collect-all', 'frictionless',  # Frictionless depends on data files
-        '--distpath', 'build/server'  # Output the file to the build folder
+        '--distpath', 'build/server',  # Output the file to the build folder
+        '--log-level', 'ERROR'
     ])
 
 
