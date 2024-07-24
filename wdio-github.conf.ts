f01dd16a45f054702a9374fc2e12f4e9e315a06e
pdelboca <patriciodelboca@gmail.com>
2024-07-24 08:43:58 +0200

    Change parameters according to Chrome documentation

--- wdio-github.conf.ts
+++ wdio-github.conf.ts
@@ -1,15 +1,16 @@
 // Run the E2E tests in Github Ubuntu machines
-// https://github.com/webdriverio/jasmine-boilerplate/commit/53e88abc19ddf407aa95806532e920b42232a8a7
-
 const basicConfig = require('./wdio.conf.ts')
 
 exports.config = {
   ...basicConfig.config,
   capabilities: [
     {
+      maxInstances: 5,
       browserName: 'chrome',
+      acceptInsecureCerts: true,
+      // https://developer.chrome.com/blog/headless-chrome/#starting_headless_cli
       'goog:chromeOptions': {
-        args: ['headless', 'disable-gpu']
+        args: ['--headless', '--disable-gpu', '--remote-debugging-port=9222'],
       },
     },
   ],
