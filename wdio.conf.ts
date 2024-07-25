4ab8d3e26461d7c39aa9aa6ed58f8d62de2c5eac
pdelboca <patriciodelboca@gmail.com>
2024-07-25 10:15:06 +0200

    Restore electron service

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -52,7 +52,10 @@ export const config: Options.Testrunner = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-        browserName: 'chrome',
+        browserName: 'electron',
+        'wdio:electronServiceOptions': {
+            appBinaryPath:'./dist/opendataeditor-linux-1.0.0.AppImage'
+        },
         'goog:chromeOptions': {
           args: ['headless', 'no-sandbox'],
         },
@@ -104,7 +107,7 @@ export const config: Options.Testrunner = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: [],
+    services: ['electron'],
 
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
