fb687c44802ca6a54908ab54943237f043934e2b
pdelboca <patriciodelboca@gmail.com>
2024-07-26 13:30:58 +0200

    Increase logs verbosity

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -66,7 +66,7 @@ export const config: Options.Testrunner = {
     // Define all options that are relevant for the WebdriverIO instance here
     //
     // Level of logging verbosity: trace | debug | info | warn | error | silent
-    logLevel: 'info',
+    logLevel: 'trace',
     //
     // Set specific log levels per logger
     // loggers:
