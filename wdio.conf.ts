d8681df121f018700e61f82648d2113c2d35c300
pdelboca <patriciodelboca@gmail.com>
2024-07-26 14:45:25 +0200

    Try another executable

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -54,7 +54,7 @@ export const config: Options.Testrunner = {
     capabilities: [{
         browserName: 'electron',
         'wdio:electronServiceOptions': {
-            appBinaryPath:'./dist/opendataeditor-linux-1.0.0.AppImage',
+            appBinaryPath:'./dist/linux-unpacked/opendataeditor',
             // https://github.com/webdriverio-community/wdio-electron-service/blob/main/docs/common-issues-debugging.md#devtoolsactiveport-file-doesnt-exist
             appArgs: ['--headless', '--no-sandbox', '--remote-debugging-port=9222']
         }
