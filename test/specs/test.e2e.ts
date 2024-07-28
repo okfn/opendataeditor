686ed826c1fe1b79b4a5f83248b645aaa7705855
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2024-07-28 20:18:16 +0200

    Add adequate test for loading screen

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -1,7 +1,11 @@
 import { $, expect } from '@wdio/globals'
 
 describe('Electron Testing', () => {
-    it('should display Open Data Editor on header', async () => {
+    it('should display loader on start', async () => {
+        const header = $('.loader')
+        await expect(header).toExist()
+    })
+    it.skip('should display Open Data Editor on header', async () => {
         const header = $('header')
         await expect(header).toHaveText(expect.stringContaining('Open Data Editor'))
     })
