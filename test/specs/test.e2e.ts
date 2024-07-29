8b0e2a5cb50bda9fe6dcbd5abd82648b4fc4d9c1
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2024-07-29 18:27:17 +0200

    Remove one test

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -1,15 +1,11 @@
 import { $, expect } from '@wdio/globals'
 
 describe('Electron Testing', () => {
-    it.skip('should display loader on start', async () => {
-        const handles = await browser.getWindowHandles()
-        await browser.switchToWindow(handles[0])
-        const header = $('.loader')
-        await expect(header).toExist()
-    })
     it('should display Open Data Editor on header', async () => {
+        // make sure we have focus on the correct window
         const handles = await browser.getWindowHandles()
         await browser.switchToWindow(handles[0])
+        
         const header = $('header')
         await expect(header).toHaveText(expect.stringContaining('Open Data Editor'))
     })
