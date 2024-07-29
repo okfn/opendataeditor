6a248421ca3481b9c86bc40eee52340af43756cf
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2024-07-29 18:06:39 +0200

    Try with original test

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -1,13 +1,15 @@
 import { $, expect } from '@wdio/globals'
 
 describe('Electron Testing', () => {
-    it('should display loader on start', async () => {
+    it.skip('should display loader on start', async () => {
         const handles = await browser.getWindowHandles()
         await browser.switchToWindow(handles[0])
         const header = $('.loader')
         await expect(header).toExist()
     })
-    it.skip('should display Open Data Editor on header', async () => {
+    it('should display Open Data Editor on header', async () => {
+        const handles = await browser.getWindowHandles()
+        await browser.switchToWindow(handles[0])
         const header = $('header')
         await expect(header).toHaveText(expect.stringContaining('Open Data Editor'))
     })
