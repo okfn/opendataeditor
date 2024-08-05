95918b844ea726a451b2406a99055c4190845727
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2024-08-05 14:13:41 +0200

    [WIP] Implement empty screen

--- client/components/Application/Content.tsx
+++ client/components/Application/Content.tsx
@@ -4,10 +4,15 @@ import { ErrorBoundary } from 'react-error-boundary'
 import File from '../Controllers/File'
 import Table from '../Controllers/Table'
 import Text from '../Controllers/Text'
-import EmptyCard from '../Parts/Cards/Empty'
 import SpinnerCard from '../Parts/Cards/Spinner'
 import * as store from '@client/store'
 import { client } from '@client/client'
+import { styled } from '@mui/material/styles'
+import Typography from '@mui/material/Typography'
+import CardContent from '@mui/material/CardContent'
+import Card from '@mui/material/Card'
+import SimpleButton from '../Parts/Buttons/SimpleButton'
+import emptyContentScreenImg from '../../assets/empty_screen.png'
 
 export default function Content() {
   const record = store.useStore((state) => state.record)
@@ -48,7 +53,27 @@ function FileContent() {
 
 function EmptyContent() {
   return (
-    <EmptyCard title="No Files Selected" description="Select a file in the left menu" />
+    <StyledCard>
+      <StyledCardContent>
+        <Box sx={{ padding: '56px 52px' }}>
+          <img src={emptyContentScreenImg} alt="Empty Content Screen" />
+        </Box>
+        <Typography sx={{ fontWeight: 600 }}>Drag & drop files or folders</Typography>
+        <Typography sx={{ fontWeight: 300 }}>
+          The ODE supports Excel & csv files
+        </Typography>
+        <Typography sx={{ fontWeight: 600 }}>Or</Typography>
+        <StyledButton
+          label={'Upload your data'}
+          sx={{ my: 0.5 }}
+          variant="contained"
+          aria-label="accept"
+        />
+        <Typography sx={{ fontWeight: 300 }}>
+          You can also add links to online tables
+        </Typography>
+      </StyledCardContent>
+    </StyledCard>
   )
 }
 
@@ -59,6 +84,45 @@ function LoadingContent() {
 // We still need to cover here and in the settings "chart" type and some other types
 // that were removed from UI in #463 because `frictionless-py` provides them
 // as `resource.datatype`
+const StyledButton = styled(SimpleButton)({
+  boxShadow: 'none',
+  width: '236px',
+  textTransform: 'none',
+  fontSize: 16,
+  padding: '18px 0',
+  lineHeight: 1.5,
+  backgroundColor: 'black',
+  fontFamily: ['Hanken Grotesk Variable', 'sans-serif'].join(','),
+  '&:hover': {
+    backgroundColor: 'gray',
+  },
+  '&:active': {
+    boxShadow: 'none',
+    backgroundColor: 'gray',
+  },
+  '&:focus': {
+    boxShadow: '0 0 0 0 rgba(0,123,255,.5)',
+  },
+})
+
+const StyledCard = styled(Card)(() => ({
+  width: '100%',
+  height: '100%',
+  border: 'none',
+  boxShadow: 'none',
+  borderRadius: 0,
+  square: 'true',
+  display: 'flex',
+  alignItems: 'center',
+}))
+
+const StyledCardContent = styled(CardContent)(() => ({
+  width: '100%',
+  display: 'flex',
+  alignItems: 'center',
+  flexDirection: 'column',
+}))
+
 export const CONTROLLERS: {
   [type: string]: React.ElementType
 } = {
