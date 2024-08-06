714958843fbb0cad1e5dacf90469035ed3c664cf
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2024-08-06 14:00:02 +0200

    Add visual elements of external url tab

--- client/components/Application/Dialogs/FileUpload.tsx
+++ client/components/Application/Dialogs/FileUpload.tsx
@@ -12,6 +12,8 @@ import uploadFilesDialogImg from '../../../assets/dialog_upload_files.png'
 import iconUploadFileImg from '../../../assets/icon_upload_file.png'
 import iconUploadFolderImg from '../../../assets/folder-open-big-plus.png'
 import Columns from '../../Parts/Grids/Columns'
+import SimpleButton from '../../Parts/Buttons/SimpleButton'
+import TextField from '@mui/material/TextField'
 
 export interface FileUploadDialogProps {
   open?: boolean
@@ -35,33 +37,6 @@ interface TabPanelProps {
   value: number
 }
 
-const StyledTab = styled(Tab)(() => ({
-  textTransform: 'none',
-  fontWeight: 600,
-  '&.Mui-selected': {
-    color: '#00D1FF',
-  },
-}))
-
-const StyledBox = styled(Box)(() => ({
-  border: '1px solid #E7E9E9',
-  borderRadius: '8px',
-  textAlign: 'center',
-  padding: '32px 48px 24px 48px',
-  color: '#717879',
-  fontSize: '14px',
-  cursor: 'pointer',
-}))
-
-const StyledSelectBox = styled(Box)(() => ({
-  marginTop: '28px',
-  backgroundColor: '#FAFAFA',
-  padding: '4px 10px',
-  borderRadius: '4px',
-  border: '0.5px solid #D3D7D8',
-  boxShadow: '0px 0px 0px 0px rgba(0, 0, 0, 0.25)',
-}))
-
 export default function FileUploadDialog(props: FileUploadDialogProps) {
   const handleCancel = () => props.onCancel && props.onCancel()
   const handleConfirm = () => props.onConfirm && props.onConfirm()
@@ -79,6 +54,8 @@ export default function FileUploadDialog(props: FileUploadDialogProps) {
     }
   }
 
+  // the event needs to be passed even if not used, disabling here so there's
+  // no unused variable error in the typescript check
   // @ts-ignore
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue)
@@ -170,21 +147,145 @@ export default function FileUploadDialog(props: FileUploadDialogProps) {
                 <img src={iconUploadFileImg} alt="Icon Upload File" />
               </Box>
               <Box>Add one or more Excel or csv files </Box>
-              <StyledSelectBox>Select F</StyledSelectBox>
+              <StyledSelectBox>
+                Select <span>F</span>
+              </StyledSelectBox>
             </StyledBox>
             <StyledBox>
               <Box>
                 <img src={iconUploadFolderImg} alt="Icon Upload File" />
               </Box>
               <Box>Add one or more folders</Box>
-              <StyledSelectBox>Select G</StyledSelectBox>
+              <StyledSelectBox>
+                Select <span>G</span>
+              </StyledSelectBox>
             </StyledBox>
           </Columns>
         </CustomTabPanel>
         <CustomTabPanel value={value} index={1}>
-          Add external data
+          <Box sx={{ paddingLeft: '140px', paddingRight: '140px' }}>
+            <Box
+              sx={{
+                fontFamily: ['Hanken Grotesk Variable', 'sans-serif'].join(','),
+                fontSize: '14px',
+              }}
+            >
+              Link to the external table:
+            </Box>
+            <StyledTextField
+              fullWidth
+              size="small"
+              name="Url"
+              label="Enter or paste URL"
+              InputLabelProps={{
+                sx: {
+                  fontFamily: ['Hanken Grotesk Variable', 'sans-serif'].join(','),
+                  fontSize: '14px',
+                  '&.MuiOutlinedInput-notchedOutline': { fontSize: '28px' },
+                },
+              }}
+              // value={props.value || ''}
+              //onChange={(ev) => props.onChange(ev.target.value)}
+              // InputProps={{ endAdornment: props.value ? <ResetButton {...props} /> : undefined }}
+              // onFocus={onFocus}
+            />
+            <StyledButton
+              label={'Add'}
+              sx={{ my: 0.5, marginTop: '53px' }}
+              variant="contained"
+              aria-label="accept"
+              onClick={() => {
+                console.log('clicked button')
+                store.openDialog('fileUpload')
+              }}
+            />
+          </Box>
         </CustomTabPanel>
       </DialogContent>
     </Dialog>
   )
 }
+
+const StyledButton = styled(SimpleButton)({
+  boxShadow: 'none',
+  width: '100%',
+  textTransform: 'none',
+  borderRadius: '9px',
+  fontSize: 14,
+  fontWeight: 700,
+  padding: '14px 24px',
+  lineHeight: 1.5,
+  backgroundColor: 'black',
+  fontFamily: ['Hanken Grotesk Variable', 'sans-serif'].join(','),
+  '&:hover': {
+    backgroundColor: 'gray',
+  },
+  '&:active': {
+    boxShadow: 'none',
+    backgroundColor: 'gray',
+  },
+  '&:focus': {
+    boxShadow: '0 0 0 0 rgba(0,123,255,.5)',
+  },
+})
+
+const StyledTab = styled(Tab)(() => ({
+  textTransform: 'none',
+  fontFamily: ['Hanken Grotesk Variable', 'sans-serif'].join(','),
+  fontWeight: 600,
+  '&.Mui-selected': {
+    color: '#00D1FF',
+  },
+}))
+
+const StyledBox = styled(Box)(() => ({
+  fontFamily: ['Hanken Grotesk Variable', 'sans-serif'].join(','),
+  border: '1px solid #E7E9E9',
+  borderRadius: '8px',
+  textAlign: 'center',
+  padding: '32px 48px 24px 48px',
+  color: '#717879',
+  fontSize: '14px',
+  cursor: 'pointer',
+}))
+
+const StyledSelectBox = styled(Box)(() => ({
+  marginTop: '28px',
+  backgroundColor: '#FAFAFA',
+  padding: '4px 10px',
+  borderRadius: '4px',
+  border: '0.5px solid #D3D7D8',
+  boxShadow: '0px 0px 0px 0px rgba(0, 0, 0, 0.25)',
+  '& span': {
+    backgroundColor: '#4C5564',
+    fontSize: '12px',
+    padding: '2px 4px',
+    borderRadius: '2px',
+    lineHeight: 1,
+    color: 'white',
+    marginLeft: '4px',
+  },
+}))
+
+const StyledTextField = styled(TextField)(() => ({
+  marginTop: '8px',
+  fontFamily: ['Hanken Grotesk Variable', 'sans-serif'].join(','),
+  fontSize: '14px',
+  '& label.Mui-focused': {
+    color: '#00D1FF',
+  },
+  '& .MuiInput-underline:after': {
+    borderBottomColor: '#00D1FF',
+  },
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: 'gray',
+    },
+    '&:hover fieldset': {
+      borderColor: '#00D1FF',
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: '#00D1FF',
+    },
+  },
+}))
