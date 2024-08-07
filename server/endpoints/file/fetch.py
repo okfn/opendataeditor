0221645f5cee8899677d138150986117120a71ff
roll <eskarev@gmail.com>
2024-08-07 11:53:21 +0100

    Extract Google Sheets title

--- server/endpoints/file/fetch.py
+++ server/endpoints/file/fetch.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import re
 import tempfile
 from pathlib import Path
 from typing import Optional
@@ -36,14 +37,9 @@ def action(project: Project, props: Props) -> Result:
     resource = FileResource(path=props.url)
 
     # Handle Googel Sheets
-    # We export Google Sheets to a temporary CSV file and read it
     if resource.format == "gsheets":
-        path = "google-sheets.csv"
-        table = TableResource(path=props.url)
-        with tempfile.NamedTemporaryFile(suffix=".csv") as file:
-            table.write(file.name)
-            resource = FileResource(path=file.name)
-            bytes = resource.read_file()
+        path = read_google_sheets_path(props.url)
+        bytes = read_google_sheets_bytes(props.url)
 
     # Handle regular files
     else:
@@ -74,3 +70,26 @@ def action(project: Project, props: Props) -> Result:
         raise Exception("The file is not tabular")
 
     return Result(path=path)
+
+
+# We export Google Sheets to a temporary CSV file and read it
+def read_google_sheets_bytes(url: str):
+    table = TableResource(path=url)
+    with tempfile.NamedTemporaryFile(suffix=".csv") as file:
+        table.write(file.name)
+        resource = FileResource(path=file.name)
+        bytes = resource.read_file()
+        return bytes
+
+
+# We use public HTML to extract the title of the document
+def read_google_sheets_path(url: str):
+    file = FileResource(path=url)
+    text = file.read_text(size=10000)
+    match = re.search(r"<title>(.*?)</title>", text)
+    path = "google-sheets.csv"
+    if match:
+        title = match.group(1).strip()
+        title = title.replace(" - Google Sheets", "")
+        path = f"{title}.csv"
+    return path
