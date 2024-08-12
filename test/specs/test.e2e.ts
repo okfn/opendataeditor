2282a27d63c334691679f25d4650692371bb7383
pdelboca <patriciodelboca@gmail.com>
2024-08-12 11:07:34 +0200

    Test welcomming screen is displayed

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -1,13 +1,20 @@
 import { $, expect } from '@wdio/globals'
 
 describe('Electron Testing', () => {
-    it('should display Open Data Editor on header', async () => {
-        // make sure we have focus on the correct window
+    it('displays welcomming screen', async () => {
+        // ODE has a Loading window and a main Window. We need to switch to the main one.
         const handles = await browser.getWindowHandles()
         await browser.switchToWindow(handles[0])
-        
-        const header = $('header')
-        await expect(header).toHaveText(expect.stringContaining('Open Data Editor'))
+
+        // Calling browser.react$(...) will fail to find the root element. So we find it explicitly.
+        // https://github.com/baruchvlz/resq/issues/31#issuecomment-493760156
+        const root = await $('#root')
+        const banner = await root.react$('WelcomeBanner')
+        await expect(banner).toHaveText(expect.stringContaining('Welcome to the Open Data Editor!'))
+        const simpleButton = await root.react$('SimpleButton', {props: {label: 'Get started'}})
+        await simpleButton.click()
+
+        browser.closeWindow()
     })
 })
 
--- wdio.conf.ts
+++ wdio.conf.ts
@@ -29,27 +29,8 @@ export const config: Options.Testrunner = {
     exclude: [
         // 'path/to/excluded/files'
     ],
-    //
-    // ============
-    // Capabilities
-    // ============
-    // Define your capabilities here. WebdriverIO can run multiple capabilities at the same
-    // time. Depending on the number of capabilities, WebdriverIO launches several test
-    // sessions. Within your capabilities you can overwrite the spec and exclude options in
-    // order to group specific specs to a specific capability.
-    //
-    // First, you can define how many instances should be started at the same time. Let's
-    // say you have 3 different capabilities (Chrome, Firefox, and Safari) and you have
-    // set maxInstances to 1; wdio will spawn 3 processes. Therefore, if you have 10 spec
-    // files and you set maxInstances to 10, all spec files will get tested at the same time
-    // and 30 processes will get spawned. The property handles how many capabilities
-    // from the same test should run tests.
-    //
-    maxInstances: 10,
-    //
-    // If you have trouble getting all important capabilities together, check out the
-    // Sauce Labs platform configurator - a great tool to configure your capabilities:
-    // https://saucelabs.com/platform/platform-configurator
+    // maxInstances > 1 will not work since port 4040 will be already occupied.
+    maxInstances: 1,
     //
     capabilities: [{
         browserName: 'electron',
