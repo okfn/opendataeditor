32a361fcdfeb5baab83ce129a6a737795d4b7767
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2024-08-16 16:10:35 +0200

    Add logo

--- client/components/Application/Sidebar.tsx
+++ client/components/Application/Sidebar.tsx
@@ -2,6 +2,7 @@ import Box from '@mui/material/Box'
 import Browser from './Browser'
 import Action from './Action'
 import { useTheme } from '@mui/material/styles'
+import sidebarLogo from '../../assets/ODE_sidebar_logo.svg'
 
 export default function Sidebar() {
   const theme = useTheme()
@@ -9,7 +10,8 @@ export default function Sidebar() {
   const contentHeight = `calc(100vh - ${theme.spacing(8 + 8 + 8)})`
 
   return (
-    <Box sx={{ height, borderRight: 'solid 1px #ddd', width: '284px' }}>
+    <Box sx={{ height, borderRight: 'solid 1px #ddd', width: '284px', padding: '24px' }}>
+      <img src={sidebarLogo} alt="" />
       <Box sx={{ height: contentHeight }}>
         <Browser />
       </Box>
