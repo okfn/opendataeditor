a965d866d02c5c20f68c3195ef6a566c93067247
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2024-08-17 21:52:01 +0200

    [WiP] Style icons of filetree sidebar

--- client/components/Parts/Trees/File.tsx
+++ client/components/Parts/Trees/File.tsx
@@ -6,7 +6,6 @@ import { TreeItem, TreeView, TreeItemProps, treeItemClasses } from '@mui/x-tree-
 import Box from '@mui/material/Box'
 import Stack from '@mui/material/Stack'
 import ScrollBox from '../Boxes/Scroll'
-import * as settings from '../../../settings'
 import * as helpers from '../../../helpers'
 import * as types from '../../../types'
 
@@ -114,10 +113,9 @@ const StyledTreeItem = styled(
 }))
 
 function TreeItemIcon(props: { nodeId: string; item: types.IFileTreeItem }) {
-  const Icon = getIcon(props.item.type)
-  let color = 'disabled'
+  let color = 'gray'
   if (props.item.type === 'folder') color = 'primary'
-  if (props.item.name) color = props.item.errors ? 'error' : 'success'
+  if (props.item.name) color = props.item.errors ? 'red' : 'green'
 
   const fontWeight = 'normal'
   // When data package is enabled consider highlighting it
@@ -130,10 +128,13 @@ function TreeItemIcon(props: { nodeId: string; item: types.IFileTreeItem }) {
         display: 'flex',
         alignItems: 'center',
         overflow: 'hidden',
-        '& svg': { mr: 1 },
+        '& div': { mr: 1 },
       }}
     >
-      <Icon color={color} />
+      <div style={{height: '8px',
+        width: '8px',
+        backgroundColor: color,
+        borderRadius: '50%',}}>{" "}</div>
       <span style={{ whiteSpace: 'nowrap', fontWeight }}>{props.item.label}</span>
     </Box>
   )
@@ -155,10 +156,6 @@ function PlusSquare(props: SvgIconProps) {
       <path d="M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 12.977h-4.923v4.896q0 .401-.281.682t-.682.281v0q-.375 0-.669-.281t-.294-.682v-4.896h-4.923q-.401 0-.682-.294t-.281-.669v0q0-.401.281-.682t.682-.281h4.923v-4.896q0-.401.294-.682t.669-.281v0q.401 0 .682.281t.281.682v4.896h4.923q.401 0 .682.281t.281.682v0q0 .375-.281.669t-.682.294z" />
     </SvgIcon>
   )
-}
-
-function getIcon(type: string): React.ElementType {
-  return settings.TYPE_ICONS[type] || settings.TYPE_ICONS.file
 }
 
 // TODO: use color from theme
