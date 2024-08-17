34e93e523d0fb3120aaa3f8bfa704ae31356f7c1
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2024-08-17 20:00:20 +0200

    Add "upload data" button to sidebar

--- client/components/Application/Sidebar.tsx
+++ client/components/Application/Sidebar.tsx
@@ -3,6 +3,8 @@ import Browser from './Browser'
 import LowerMenu from './LowerMenu'
 import { useTheme } from '@mui/material/styles'
 import sidebarLogo from '../../assets/ODE_sidebar_logo.svg'
+import Button from '@mui/material/Button'
+import * as store from '@client/store'
 
 export default function Sidebar() {
   const theme = useTheme()
@@ -20,10 +22,17 @@ export default function Sidebar() {
         padding: '24px',
       }}
     >
-      <Box sx={{ height: '32px' }}>
+      <Box sx={{ height: '32px', marginBottom: '26px' }}>
         {' '}
         <img src={sidebarLogo} alt="" />
       </Box>
+      <Button
+        variant="outlined"
+        sx={{ textTransform: 'none' }}
+        onClick={() => store.openDialog('fileUpload')}
+      >
+        Upload your data
+      </Button>
       <Box sx={{ height: contentHeight }}>
         <Browser />
       </Box>
