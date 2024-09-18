178ced64bd290290069d2f0624e4159ed013dfc6
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2024-09-18 19:30:00 +0200

    Disable multi select

--- client/components/Application/Browser.tsx
+++ client/components/Application/Browser.tsx
@@ -23,7 +23,7 @@ function DefaultBrowser() {
   const path = store.useStore((state) => state.path)
   const files = store.useStore((state) => state.files)
   const event = store.useStore((state) => state.event)
-  const selectedMultiplePaths = store.useStore((state) => state.selectedMultiplePaths)
+  // const selectedMultiplePaths = store.useStore((state) => state.selectedMultiplePaths)
 
   return (
     <ErrorBoundary
@@ -48,12 +48,13 @@ function DefaultBrowser() {
       <FileTree
         files={files}
         event={event}
-        selectedMultiple={selectedMultiplePaths}
+        // selectedMultiple={selectedMultiplePaths}
         selected={path}
-        onSelect={(paths) =>
-          paths.length <= 1
-            ? store.selectFile({ path: paths[0] })
-            : store.selectMultipleFiles(paths)
+        onSelect={(path) =>
+          // paths.length <= 1
+          //   ? store.selectFile({ path: paths[0] })
+          //   : store.selectMultipleFiles(paths)
+          store.selectFile({ path })
         }
       />
     </ErrorBoundary>
