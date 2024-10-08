0d3a30fb58eb52e8485a04b18bf02c1bcfc97d29
roll <eskarev@gmail.com>
2024-10-08 16:29:17 +0100

    Implemented License dialog

--- client/components/Editors/Resource/Sections/Licenses.tsx
+++ client/components/Editors/Resource/Sections/Licenses.tsx
@@ -5,6 +5,13 @@ import InputField from '../../../Parts/Fields/Input'
 import EditorItem from '../../Base/Item'
 import EditorList from '../../Base/List'
 import EditorListItem from '../../Base/ListItem'
+import Dialog from '@mui/material/Dialog'
+import DialogContent from '@mui/material/DialogContent'
+import DialogTitle from '@mui/material/DialogTitle'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select, { SelectChangeEvent } from '@mui/material/Select'
 import { useStore, selectors, select } from '../store'
 import validator from 'validator'
 
@@ -14,24 +21,72 @@ export default function Licenses() {
 }
 
 function LicenseList() {
+  const [dialogOpen, setDialogOpen] = React.useState(false)
+
   const query = useStore((state) => state.licenseState.query)
   const licenseItems = useStore(selectors.licenseItems)
   const updateLicenseState = useStore((state) => state.updateLicenseState)
-  const addLicense = useStore((state) => state.addLicense)
   const removeLicense = useStore((state) => state.removeLicense)
+
   return (
-    <EditorList kind="license" query={query} onAddClick={() => addLicense()}>
-      {licenseItems.map(({ index, license }) => (
-        <EditorListItem
-          key={index}
-          kind="license"
-          name={license.name}
-          type="license"
-          onClick={() => updateLicenseState({ index })}
-          onRemoveClick={() => removeLicense(index)}
-        />
-      ))}
-    </EditorList>
+    <>
+      <EditorList kind="license" query={query} onAddClick={() => setDialogOpen(true)}>
+        {licenseItems.map(({ index, license }) => (
+          <EditorListItem
+            key={index}
+            kind="license"
+            name={license.name}
+            type="license"
+            onClick={() => updateLicenseState({ index })}
+            onRemoveClick={() => removeLicense(index)}
+          />
+        ))}
+      </EditorList>
+      <LicenseDialog open={dialogOpen} onClose={() => setDialogOpen(false)} />
+    </>
+  )
+}
+
+function LicenseDialog(props: { open: boolean; onClose: () => void }) {
+  const addLicense = useStore((state) => state.addLicense)
+
+  const licenses = [
+    { name: 'ODC-By-1.0', title: 'Open Data Commons Attribution License' },
+    { name: 'ODbL-1.0', title: 'Open Data Commons Open Database License' },
+    { name: 'PDDL-1.0', title: 'Open Data Commons Public Domain Dedication and Licence' },
+    { name: 'Custom', title: 'I will add a license manually' },
+  ]
+
+  const handleSelect = (event: SelectChangeEvent) => {
+    props.onClose()
+  }
+
+  return (
+    <Box>
+      <Dialog disableEscapeKeyDown open={props.open} onClose={props.onClose}>
+        <DialogTitle>Select the license</DialogTitle>
+        <DialogContent>
+          <Box component="form">
+            <FormControl sx={{ my: 1, minWidth: '30em' }}>
+              <InputLabel id="license-dialog-select-label-id">License</InputLabel>
+              <Select
+                value=""
+                labelId="demo-simple-select-label"
+                id="license-dialog-select-id"
+                label="License"
+                onChange={handleSelect}
+              >
+                {licenses.map((license) => (
+                  <MenuItem key={license.name} value={license.name}>
+                    {license.title}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Box>
+        </DialogContent>
+      </Dialog>
+    </Box>
   )
 }
 
