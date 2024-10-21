65609bc4de84d512c0b0f70efccc7a21e7c1973e
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2024-10-21 17:55:30 +0200

    Remove help box in publish dialog

--- client/components/Editors/Portal/Layout.tsx
+++ client/components/Editors/Portal/Layout.tsx
@@ -1,16 +1,13 @@
 import Box from '@mui/material/Box'
 import publishDialogImg from '../../Application/Dialogs/assets/dialog_publish.png'
-import EditorHelp from '../Base/Help'
 import CkanSection from './Sections/Ckan'
 import GithubSection from './Sections/Github'
 import ZenodoSection from './Sections/Zenodo'
-import { useStore } from './store'
 import DialogTabs from '../../Parts/Tabs/Dialog'
 
 export default function Layout() {
   const tabLabels = ['Ckan', 'Github', 'Zenodo']
 
-  const helpItem = useStore((state) => state.helpItem)
   return (
     <Box sx={{ height: '100%' }}>
       <Box
@@ -26,15 +23,12 @@ export default function Layout() {
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <DialogTabs labels={tabLabels}>
           <Box>
-            <EditorHelp helpItem={helpItem} withIcon />
             <CkanSection />
           </Box>
           <Box>
-            <EditorHelp helpItem={helpItem} withIcon />
             <GithubSection />
           </Box>
           <Box>
-            <EditorHelp helpItem={helpItem} withIcon />
             <ZenodoSection />
           </Box>
         </DialogTabs>
