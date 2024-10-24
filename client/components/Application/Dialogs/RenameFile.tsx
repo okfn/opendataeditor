e1f26e0648d9d6b3bc8c07b61576577d50aa3ceb
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2024-10-24 16:06:10 +0200

    Add case for folders

--- client/components/Application/Dialogs/RenameFile.tsx
+++ client/components/Application/Dialogs/RenameFile.tsx
@@ -3,17 +3,23 @@ import * as store from '@client/store'
 
 export default function RenameFileDialog() {
   const currentFilePath = store.useStore((state) => state.path)
+  const isFolder = store.useStore(store.getIsFolder)
 
   return (
     <InputDialog
       open={true}
-      value={currentFilePath}
+      value=""
       title="Rename File"
       label="Save"
-      placholder="Name of new file"
+      placholder={`Name of new ${isFolder ? 'folder' : 'file'}`}
       onCancel={store.closeDialog}
       onConfirm={async (newPath) => {
-        if (currentFilePath) await store.moveFile(currentFilePath, newPath)
+        if (currentFilePath)
+          if (!isFolder) {
+            await store.moveFile(currentFilePath, newPath)
+          } else {
+            await store.moveFolder(currentFilePath, newPath)
+          }
         store.closeDialog()
       }}
     />
