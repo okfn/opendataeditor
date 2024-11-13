1d63e2abef420a7cceb18d438914d88b9a91389a
roll <eskarev@gmail.com>
2024-11-13 09:16:37 +0000

    Added first step to AI Assistant

--- client/components/Application/Dialogs/Assistant/Assistant.tsx
+++ client/components/Application/Dialogs/Assistant/Assistant.tsx
@@ -1,16 +1,21 @@
+import TwoButtonDialog from '@client/components/Parts/Dialogs/TwoButton'
 import * as appStore from '@client/store'
-import Dialog from '@mui/material/Dialog'
+import AutoFixHighIcon from '@mui/icons-material/AutoFixHigh'
 
 export function AssistantDialog() {
   return (
-    <Dialog
-      fullWidth
+    <TwoButtonDialog
       open={true}
-      aria-labelledby="AI"
-      aria-describedby="AI Assistant"
-      onClose={appStore.closeDialog}
+      maxWidth="md"
+      title="AI Assistant"
+      Icon={AutoFixHighIcon}
+      cancelLabel="Cancel"
+      onCancel={appStore.closeDialog}
+      onConfirm={console.log}
     >
-      AI Assistant
-    </Dialog>
+      If you proceed, the Open Data Editor will only share the names of the columns in
+      your table to suggest improvements to the titles and descriptions associated with
+      them. Do you want to proceed?
+    </TwoButtonDialog>
   )
 }
