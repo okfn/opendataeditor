29bf5b9546b06988d761228cbfe5c0a61017bd98
roll <eskarev@gmail.com>
2024-11-26 15:52:32 +0000

    Fixed SaveChanges

--- client/components/Application/Dialogs/SaveChanges/SaveChanges.tsx
+++ client/components/Application/Dialogs/SaveChanges/SaveChanges.tsx
@@ -1,16 +1,9 @@
 import NoButtonDialog from '@client/components/Parts/Dialogs/NoButton'
 import { LinearProgress } from '@client/components/Parts/Progress/Linear'
-import * as appStore from '@client/store'
-import * as React from 'react'
 import * as store from './store'
 
 export function SaveChangesDialog() {
-  const dialog = appStore.useStore((state) => state.dialog)
   const { progress } = store.useState()
-
-  React.useEffect(() => {
-    store.resetState()
-  }, [dialog])
 
   return (
     <NoButtonDialog
