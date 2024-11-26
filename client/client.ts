fe31426c7ee88dee4fe8d6e40b3171b10ff69b9a
roll <eskarev@gmail.com>
2024-11-26 13:00:19 +0000

    Added debug

--- client/client.ts
+++ client/client.ts
@@ -313,6 +313,7 @@ async function makeRequest<T>(
     const message = data?.detail || `code (${response.status})`
     throw new Error(message)
   } catch (error: any) {
+    console.error(error)
     const detail = error?.message || error.toString()
     return new ClientError(detail)
   }
