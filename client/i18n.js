301fba7f2f6743b3064e472d3ce611451181d8ad
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2024-12-01 03:28:46 +0100

    Basic setup and example

---
+++ client/i18n.js
@@ -0,0 +1,14 @@
+import i18next from 'i18next'
+import { initReactI18next } from 'react-i18next'
+
+i18next
+    .use(initReactI18next)
+    .init({ fallbackLng : 'en',
+        resources: {
+            en: {
+                translation: {
+                    'get-started': 'Get Started'
+                }
+            }
+        }
+    })
