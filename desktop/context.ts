4d52476aae0120a97ee5346384de96e077fb6aea
roll <eskarev@gmail.com>
2024-12-02 14:19:24 +0000

    Added ability to copy selected text

--- desktop/context.ts
+++ desktop/context.ts
@@ -3,12 +3,19 @@ import { BrowserWindow, Menu, MenuItem } from 'electron'
 export function createContextMenu(mainWindow: BrowserWindow) {
   mainWindow.webContents.on('context-menu', (_, props) => {
     const menu = new Menu()
-    if (props.isEditable) {
-      menu.append(new MenuItem({ label: 'Cut', role: 'cut' }))
+
+    if (props.selectionText) {
       menu.append(new MenuItem({ label: 'Copy', role: 'copy' }))
+    }
+
+    if (props.isEditable) {
+      menu.append(new MenuItem({ label: 'Copy', role: 'copy' }))
+      menu.append(new MenuItem({ label: 'Cut', role: 'cut' }))
       menu.append(new MenuItem({ label: 'Paste', role: 'paste' }))
     }
 
-    menu.popup()
+    if (menu.items.length > 0) {
+      menu.popup()
+    }
   })
 }
