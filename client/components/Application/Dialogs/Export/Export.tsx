d05e65d85ceef9398d54d7d19b72dac68812c15f
roll <eskarev@gmail.com>
2024-12-24 10:30:53 +0000

    Implemented format selector

--- client/components/Application/Dialogs/Export/Export.tsx
+++ client/components/Application/Dialogs/Export/Export.tsx
@@ -1,17 +1,30 @@
 import TwoButtonDialog from '@client/components/Parts/Dialogs/TwoButton'
 import * as appStore from '@client/store'
+import Stack from '@mui/material/Stack'
+import Typography from '@mui/material/Typography'
 import * as React from 'react'
 import { useTranslation } from 'react-i18next'
+import SelectField from '../../../Parts/Fields/Select'
+import { formats } from './constants'
 import * as store from './store'
+import { IFormat } from './types'
 
 export function ExportDialog() {
+  const { t } = useTranslation()
+  const { format } = store.useState()
   const dialog = appStore.useStore((state) => state.dialog)
 
   React.useEffect(() => {
     store.resetState()
   }, [dialog])
 
-  const { t } = useTranslation()
+  const handleChange = (value: string) => {
+    store.setFormat(value as IFormat)
+  }
+
+  const options = formats.map((format) => {
+    return { value: format.name, label: t(format.title) }
+  })
 
   return (
     <TwoButtonDialog
@@ -22,7 +35,17 @@ export function ExportDialog() {
       onCancel={store.closeDialog}
       onConfirm={store.closeDialog}
     >
-      <div>export</div>
+      <Stack>
+        <SelectField
+          label={t('format')}
+          value={format}
+          options={options}
+          onChange={(value) => handleChange(value)}
+        />
+        <Typography sx={{ fontStyle: 'italic' }}>
+          {t('select-metadata-format')}.
+        </Typography>
+      </Stack>
     </TwoButtonDialog>
   )
 }
--- client/components/Application/Dialogs/Export/store.ts
+++ client/components/Application/Dialogs/Export/store.ts
@@ -1,8 +1,10 @@
 import * as helpers from '@client/helpers'
 import * as appStore from '@client/store'
 import * as types from '@client/types'
+import { IFormat } from './types'
 
 class State {
+  format: IFormat = 'resource'
   progress?: types.IProgress
 }
 
@@ -10,6 +12,10 @@ export const { state, useState, resetState } = helpers.createState(
   'ExportDialog',
   new State()
 )
+
+export function setFormat(format: IFormat) {
+  state.format = format
+}
 
 export function closeDialog() {
   if (!state.progress?.blocking) {
