87d83ea2df47d7623ba41b9d9e48399ff97a3afa
roll <roll@users.noreply.github.com>
2024-12-26 08:54:36 +0000

    Improve report panel large cells handling (#698)
    * Improve report panel large cells handling
    
    * Increased max tooltip width
    
    * Fixed geometry
    
    * Skip false positive failing tests

--- server/endpoints/file/__spec__/test_fetch.py
+++ server/endpoints/file/__spec__/test_fetch.py
@@ -8,7 +8,8 @@ from server.fixtures import folder1, not_secure, url1, url1bytes, url1name
 # Action
 
 
-@pytest.mark.skip
+@pytest.mark.skip(reason="Pytest-vcr new version bug")
+@pytest.mark.vcr
 def test_server_file_fetch(client):
     client("/file/fetch", url=url1)
     assert client("/file/read", path=url1name).bytes == url1bytes
@@ -17,7 +18,8 @@ def test_server_file_fetch(client):
     ]
 
 
-@pytest.mark.skip
+@pytest.mark.skip(reason="Pytest-vcr new version bug")
+@pytest.mark.vcr
 def test_server_file_fetch_to_folder(client):
     client("/folder/create", path=folder1)
     path = client("/file/fetch", url=url1, folder=folder1).path
