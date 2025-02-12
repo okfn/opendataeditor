a69a554d668d92281072da3017ed37b543bed939
pdelboca <patriciodelboca@gmail.com>
2025-02-12 09:54:08 +0100

    Bump version to 1.4.0

--- ode/__init__.py
+++ ode/__init__.py
@@ -1,2 +1,3 @@
-# Required for PyInstaller to collect all assets
-__version__ = "1.3.0"
+# ODE Module was created for PyInstaller to collect all assets of the project properly
+
+__version__ = "1.4.0"
