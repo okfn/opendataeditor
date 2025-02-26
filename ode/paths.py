d711b4b04fac1deead2c4bd74493289a8d5c05b1
Nicco Kunzmann <niccokunzmann@rambler.ru>
2025-02-26 14:02:36 +0100

    Delete Matedata as well as file (#742)
    Move get_path_to_metadata_file into Paths

--- ode/paths.py
+++ ode/paths.py
@@ -21,3 +21,26 @@ class Paths:
     @classmethod
     def translation(cls, filename):
         return os.path.join(cls.assets, "translations", filename)
+
+    @classmethod
+    def get_path_to_metadata_file(cls, filepath: str|Path) -> Path:
+        """Returns the path to the metadata file of the given file.
+
+        Metadata is a JSON object that stores Fricionless Metadata and any other
+        metadata required by ODE. All metadata files are going to be stored in a
+        `.metadata` folder mimicing the file name and the structure of the project
+        folder.
+
+        Example 1:
+          - File: Paths.PROJECT_FOLDER / 'myfile.csv'
+          - Metadata: Paths.PROJECT_FOLDER / '.metadata/myfile.json'
+
+        Example 2 (subfolder):
+          - File: Paths.PROJECT_FOLDER / 'subfolder/invalid-file.csv'
+          - Metadata: Paths.PROJECT_FOLDER / '.metadata/subfolder/invalid-file.json'
+        """
+        filepath = Path(filepath) if isinstance(filepath, str) else filepath
+        relative_path = filepath.parent.relative_to(cls.PROJECT_PATH)
+        metadata_path = cls.METADATA_PATH / relative_path
+        metadata_filepath = metadata_path / (filepath.stem + '.json')
+        return metadata_filepath
