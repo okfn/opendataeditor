b730b3d5d07d7de14e29d6a2309436580d2f8f0d
Bernhard Krabina <bernhard.krabina@km-a.net>
2025-03-07 10:56:47 +0100

    Create de.ts
    add German translation

---
+++ ode/assets/translations/de.ts
@@ -0,0 +1,337 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="1.0" language="de_DE">
+<context>
+    <name>ChatGPTDialog</name>
+    <message>
+        <location filename="../../panels/ai.py" line="62"/>
+        <location filename="../../panels/ai.py" line="70"/>
+        <source>Input Error</source>
+        <translation>Eingabefehler</translation>
+    </message>
+    <message>
+        <location filename="../../panels/ai.py" line="64"/>
+        <source>A data file must be selected in order to use the AI tool.</source>
+        <translation>Eine Datei muss ausgewählt werden, um das AI-Tool zu verwenden.</translation>
+    </message>
+    <message>
+        <location filename="../../panels/ai.py" line="71"/>
+        <source>API Key and Prompt are required!</source>
+        <translation>API-Schlüssel und Prompt sind erforderlich!</translation>
+    </message>
+    <message>
+        <location filename="../../panels/ai.py" line="91"/>
+        <source>API Error</source>
+        <translation>API-Fehler</translation>
+    </message>
+    <message>
+        <location filename="../../panels/ai.py" line="91"/>
+        <source>Error: {str(e)}</source>
+        <translation>Fehler: {str(e)}</translation>
+    </message>
+    <message>
+        <location filename="../../panels/ai.py" line="94"/>
+        <source>Please enter your OpenAI API Key: </source>
+        <translation>Bitte geben Sie Ihren OpenAI API-Schlüssel ein: </translation>
+    </message>
+    <message>
+        <location filename="../../panels/ai.py" line="96"/>
+        <source>Click &lt;a href=&apos;https://help.openai.com/en/articles/4936850-where-do-i-find-my-openai-api-key&apos;&gt;here&lt;/a&gt; to learn how to find your key.
+You can also check OpenAI terms and policies &lt;a href=&apos;https://openai.com/policies/&apos;&gt;here&lt;/a&gt;.</source>
+        <translation>Klicken Sie &lt;a href=&apos;https://help.openai.com/en/articles/4936850-where-do-i-find-my-openai-api-key&apos;&gt;hier&lt;/a&gt;, um zu erfahren, wie Sie Ihren Schlüssel finden.
+Sie können auch die Nutzungsbedingungen und Richtlinien von OpenAI &lt;a href=&apos;https://openai.com/policies/&apos;&gt;hier&lt;/a&gt; überprüfen.</translation>
+    </message>
+    <message>
+        <location filename="../../panels/ai.py" line="97"/>
+        <source>Please enter your prompt to the AI assistant: </source>
+        <translation>Bitte geben Sie Ihren Prompt für den KI-Assistenten ein: </translation>
+    </message>
+    <message>
+        <location filename="../../panels/ai.py" line="98"/>
+        <source>Result: </source>
+        <translation>Ergebnis: </translation>
+    </message>
+    <message>
+        <location filename="../../panels/ai.py" line="99"/>
+        <source>Submit</source>
+        <translation>Senden</translation>
+    </message>
+    <message>
+        <location filename="../../panels/ai.py" line="100"/>
+        <source>Data Privacy</source>
+        <translation>Datenschutz</translation>
+    </message>
+    <message>
+        <location filename="../../panels/ai.py" line="102"/>
+        <source>Open Data Editor will only share the names of the columns in your table to suggest
+improvements to the titles and descriptions associated with them.</source>
+        <translation>Open Data Editor wird nur die Namen der Spalten in Ihrer Tabelle freigeben, um
+Verbesserungsvorschläge für die Titel und Beschreibungen bereitzustellen, die mit ihnen verknüpft sind.</translation>
+    </message>
+</context>
+<context>
+    <name>DataUploadDialog</name>
+    <message>
+        <location filename="../../dialogs/upload.py" line="148"/>
+        <source>Please paste a valid URL.</source>
+        <translation>Bitte fügen Sie eine gültige URL ein.</translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/upload.py" line="151"/>
+        <source>Please paste a valid URL starting with http:// or https://.</source>
+        <translation>Bitte fügen Sie eine gültige URL ein, die mit http:// oder https:// beginnt.</translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/upload.py" line="169"/>
+        <source>Error: The URL is not associated with a table</source>
+        <translation>Fehler: Die URL ist nicht mit einer Tabelle verknüpft</translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/upload.py" line="187"/>
+        <source>Upload your data</source>
+        <translation>Laden Sie Ihre Daten hoch</translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/upload.py" line="188"/>
+        <source>Add one or more Excel or csv files</source>
+        <translation>Fügen Sie eine oder mehrere Excel- oder CSV-Dateien hinzu</translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/upload.py" line="189"/>
+        <source>Add one or more folders</source>
+        <translation>Fügen Sie einen oder mehrere Ordner hinzu</translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/upload.py" line="190"/>
+        <location filename="../../dialogs/upload.py" line="191"/>
+        <source>Select</source>
+        <translation>Auswählen</translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/upload.py" line="192"/>
+        <source>Link to the external table: </source>
+        <translation>Link zur externen Tabelle: </translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/upload.py" line="193"/>
+        <source>Enter or paste URL</source>
+        <translation>URL eingeben oder einfügen</translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/upload.py" line="194"/>
+        <source>Here you can paste links from public Google Sheets and urls from csv files in open data portals and GitHub.</source>
+        <translation>Hier können Sie Links von öffentlichen Google Tabellen und URLs von CSV-Dateien in Open-Data-Portalen und GitHub einfügen.</translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/upload.py" line="195"/>
+        <source>Add</source>
+        <translation>Hinzufügen</translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/upload.py" line="196"/>
+        <source>From Your Computer</source>
+        <translation>Von Ihrem Computer</translation>
+    </message>
+    <message>
+        <location filename="../../dialogs/upload.py" line="197"/>
+        <source>Add External Data</source>
+        <translation>Externe Daten hinzufügen</translation>
+    </message>
+</context>
+<context>
+    <name>DataViewer</name>
+    <message>
+        <location filename="../../panels/data.py" line="226"/>
+        <source>No file selected or Preview not available for this file.</source>
+        <translation>Keine Datei ausgewählt oder Vorschau nicht verfügbar für diese Datei.</translation>
+    </message>
+</context>
+<context>
+    <name>ErrorsWidget</name>
+    <message>
+        <location filename="../../panels/errors.py" line="153"/>
+        <source>No errors to show.</source>
+        <translation>Keine Fehler zum Anzeigen.</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Rename</source>
+        <translation type="vanished">Umbenennen</translation>
+    </message>
+    <message>
+        <source>Enter new name:</source>
+        <translation type="vanished">Neuen Namen eingeben:</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation type="vanished">Fehler</translation>
+    </message>
+    <message>
+        <source>Source is a file but destination a directory.</source>
+        <translation type="vanished">Die Quelle ist eine Datei, das Ziel jedoch ein Verzeichnis.</translation>
+    </message>
+    <message>
+        <source>Source is a directory but destination a file.</source>
+        <translation type="vanished">Die Quelle ist ein Verzeichnis, das Ziel jedoch eine Datei.</translation>
+    </message>
+    <message>
+        <source>Operation not permitted.</source>
+        <translation type="vanished">Vorgang nicht zulässig.</translation>
+    </message>
+    <message>
+        <source>Error: {e}</source>
+        <translation type="vanished">Fehler: {e}</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation type="vanished">Löschen</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to delete this?</source>
+        <translation type="vanished">Sind Sie sicher, dass Sie dies löschen möchten?</translation>
+    </message>
+    <message>
+        <source>Failed to delete: {e}</source>
+        <translation type="vanished">Löschen fehlgeschlagen: {e}</translation>
+    </message>
+    <message>
+        <source>Upload your data</source>
+        <translation type="vanished">Laden Sie Ihre Daten hoch</translation>
+    </message>
+    <message>
+        <source>User guide</source>
+        <translation type="vanished">Benutzerhandbuch</translation>
+    </message>
+    <message>
+        <source>Report an issue</source>
+        <translation type="vanished">Ein Problem melden</translation>
+    </message>
+    <message>
+        <source>Data</source>
+        <translation type="vanished">Daten</translation>
+    </message>
+    <message>
+        <source>Metadata</source>
+        <translation type="vanished">Metadaten</translation>
+    </message>
+    <message>
+        <source>Errors Report</source>
+        <translation type="vanished">Fehlerbericht</translation>
+    </message>
+    <message>
+        <source>Source</source>
+        <translation type="vanished">Quelle</translation>
+    </message>
+    <message>
+        <source>Publish</source>
+        <translation type="vanished">Veröffentlichen</translation>
+    </message>
+    <message>
+        <source>Save changes</source>
+        <translation type="vanished">Änderungen speichern</translation>
+    </message>
+    <message>
+        <source>AI</source>
+        <translation type="vanished">KI</translation>
+    </message>
+    <message>
+        <location filename="../../main.py" line="479"/>
+        <source>File</source>
+        <translation>Datei</translation>
+    </message>
+    <message>
+        <location filename="../../main.py" line="480"/>
+        <source>Open</source>
+        <translation>Öffnen</translation>
+    </message>
+    <message>
+        <location filename="../../main.py" line="481"/>
+        <source>Edit</source>
+        <translation>Bearbeiten</translation>
+    </message>
+    <message>
+        <location filename="../../main.py" line="482"/>
+        <source>View</source>
+        <translation>Ansicht</translation>
+    </message>
+    <message>
+        <location filename="../../main.py" line="483"/>
+        <source>Help</source>
+        <translation>Hilfe</translation>
+    </message>
+    <message>
+        <location filename="../../main.py" line="567"/>
+        <source>Loading...</source>
+        <translation>Laden...</translation>
+    </message>
+    <message>
+        <source>Open File in Location</source>
+        <translation type="vanished">Datei im Speicherort öffnen</translation>
+    </message>
+    <message>
+        <source>Given the file size this operation might take some time...</source>
+        <translation type="vanished">Aufgrund der Dateigröße kann dieser Vorgang einige Zeit in Anspruch nehmen...</translation>
+    </message>
+</context>
+<context>
+    <name>Sidebar</name>
+    <message>
+        <location filename="../../main.py" line="110"/>
+        <source>Upload your data</source>
+        <translation>Laden Sie Ihre Daten hoch</translation>
+    </message>
+    <message>
+        <location filename="../../main.py" line="111"/>
+        <source>User guide</source>
+        <translation>Benutzerhandbuch</translation>
+    </message>
+    <message>
+        <location filename="../../main.py" line="112"/>
+        <source>Report an issue</source>
+        <translation>Ein Problem melden</translation>
+    </message>
+    <message>
+        <location filename="../../main.py" line="113"/>
+        <location filename="../../main.py" line="154"/>
+        <source>Rename</source>
+        <translation>Umbenennen</translation>
+    </message>
+    <message>
+        <location filename="../../main.py" line="114"/>
+        <source>Open File in Location</source>
+        <translation>Datei im Speicherort öffnen</translation>
+    </message>
+    <message>
+        <location filename="../../main.py" line="115"/>
+        <location filename="../../main.py" line="193"/>
+        <source>Delete</source>
+        <translation>Löschen</translation>
+    </message>
+    <message>
+        <location filename="../../main.py" line="154"/>
+        <source>Enter new name:</source>
+        <translation>Neuen Namen eingeben:</translation>
+    </message>
+    <message>
+        <location filename="../../main.py" line="162"/>
+        <location filename="../../main.py" line="166"/>
+        <location filename="../../main.py" line="169"/>
+        <location filename="../../main.py" line="171"/>
+        <location filename="../../main.py" line="203"/>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <location filename="../../main.py" line="163"/>
+        <source>Source is a file but destination a directory.</source>
+        <translation>Die Quelle ist eine Datei, das Ziel jedoch ein Verzeichnis.</translation>
+    </message>
+    <message>
+        <location filename="../../main.py" line="166"/>
+        <source>Source is a directory but destination a file.</source>
+        <translation>Die Quelle ist ein Verzeichnis, das Ziel jedoch eine Datei.</translation>
+    </message>
+	</context>
+</TS>
