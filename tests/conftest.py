20ab88ef26994e338f83bf781bf6f0f0b0af1210
pdelboca <patriciodelboca@gmail.com>
2025-03-25 17:23:31 +0100

    Add initial application tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,18 @@
 import pytest
-import ode
+from ode import paths, main
 
 
 @pytest.fixture(autouse=True)
 def project_folder(tmp_path):
     # Patch the PROJECT_PATH to use temporary directory
-    ode.paths.PROJECT_PATH = tmp_path
-    ode.paths.METADATA_PATH = (tmp_path / ".metadata")
+    paths.PROJECT_PATH = tmp_path
+    paths.METADATA_PATH = (tmp_path / ".metadata")
     return tmp_path
+
+
+@pytest.fixture(autouse=True)
+def window(qtbot, project_folder):
+    win = main.MainWindow()
+    qtbot.addWidget(win)
+    win.show()
+    return win
