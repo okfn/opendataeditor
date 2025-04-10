3002b22d325de456022cf492d9607f2df9c530c0
pdelboca <patriciodelboca@gmail.com>
2025-04-10 15:42:30 +0200

    Use ruff formatter

--- ode/panels/source.py
+++ ode/panels/source.py
@@ -14,6 +14,7 @@ class SourceViewer(QWidget):
     UTF-8 and ISO-8859-1 will cover most of our scenarios but we will fail
     to display source of minority languages.
     """
+
     def __init__(self):
         super().__init__()
 
@@ -37,20 +38,20 @@ class SourceViewer(QWidget):
     def open_file(self, filepath):
         """Reads the file and sets the QPlainText."""
         info = QFileInfo(filepath)
-        if not info.isFile() or info.suffix() not in ['csv']:
+        if not info.isFile() or info.suffix() not in ["csv"]:
             self.label.show()
             self.text_edit.hide()
             return
 
         content = ""
         try:
-            with open(filepath, 'r', encoding="utf-8") as file:
+            with open(filepath, "r", encoding="utf-8") as file:
                 content = file.read()
         except Exception as e:
             content = f"Error while reading the file: {e}"
 
         try:
-            with open(filepath, 'r', encoding="iso-8859-1") as file:
+            with open(filepath, "r", encoding="iso-8859-1") as file:
                 content = file.read()
         except Exception as e:
             content += f"\nError while reading the file: {e}"
