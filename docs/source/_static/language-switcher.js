53eed5099f147c48a81aa41de8786f6f65e4a95f
Patricio Del Boca <patriciodelboca@gmail.com>
2026-02-19 17:30:30 +0100

    Cleanup no longer needed language-switcher.js

--- docs/source/_static/language-switcher.js
+++
@@ -1,8 +0,0 @@
-function switchLanguage(path) {
-    // No persistence - just navigate based on path
-    // Normalize path
-    let targetPath = path;
-
-    // Navigate to new language version
-    window.location.href = targetPath;
-}
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,6 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
 
 # -- Internationalization  ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/advanced/intl.html#translating-with-sphinx-intl
